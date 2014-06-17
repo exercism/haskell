7df557a2e62fc2e307ebc160ca72edc0b7742d07
Bob Ippolito <bob@redivi.com>
2014-06-17 13:26:47 -0700

    implement list-ops for haskell

---
+++ list-ops/example.hs
@@ -0,0 +1,46 @@
+module ListOps
+  ( length
+  , reverse
+  , map
+  , filter
+  , foldr
+  , foldl'
+  , (++)
+  , concat
+  ) where
+
+import Prelude hiding
+  ( length, reverse, map, filter, foldr, (++), concat )
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' f = go
+  where
+    go acc _  | seq acc False = undefined
+    go acc [] = acc
+    go acc (x : xs) = go (f acc x) xs
+
+foldr :: (a -> b -> b) -> b -> [a] -> b
+foldr f x0 = go
+  where
+    go [] = x0
+    go (x : xs) = x `f` go xs
+
+length :: [a] -> Int
+length = foldl' (\acc _ -> 1 + acc) 0
+
+reverse :: [a] -> [a]
+reverse = foldl' (\acc x -> x : acc) []
+
+map :: (a -> b) -> [a] -> [b]
+map f = foldr (\x acc -> f x : acc) []
+
+filter :: (a -> Bool) -> [a] -> [a]
+filter f = foldr go []
+  where go x acc | f x       = x : acc
+                 | otherwise = acc
+
+(++) :: [a] -> [a] -> [a]
+xs ++ ys = foldr (:) ys xs
+
+concat :: [[a]] -> [a]
+concat = foldr (++) []
---
+++ list-ops/list-ops_test.hs
@@ -0,0 +1,68 @@
+import Test.HUnit (Assertion, (@=?), runTestTT, Test(..), Counts(..))
+import System.Exit (ExitCode(..), exitWith)
+import qualified ListOps as L
+
+exitProperly :: IO Counts -> IO ()
+exitProperly m = do
+  counts <- m
+  exitWith $ if failures counts /= 0 || errors counts /= 0 then ExitFailure 1 else ExitSuccess
+
+testCase :: String -> Assertion -> Test
+testCase label assertion = TestLabel label (TestCase assertion)
+
+main :: IO ()
+main = exitProperly $ runTestTT $ TestList
+       [ TestList listOpsTests ]
+
+big :: Int
+big = 100000
+
+listOpsTests :: [Test]
+listOpsTests =
+  [ testCase "length of empty list" $ do
+    0 @=? L.length ([] :: [Int])
+  , testCase "length of non-empty list" $ do
+    4 @=? L.length [1 .. 4 :: Int]
+  , testCase "length of large list" $ do
+    big @=? L.length [1 .. big :: Int]
+  , testCase "reverse of empty list" $ do
+    [] @=? L.reverse ([] :: [Int])
+  , testCase "reverse of non-empty list" $ do
+    [100 , 99 .. 1] @=? L.reverse [1 .. 100 :: Int]
+  , testCase "map of empty list" $ do
+    [] @=? L.map (+1) ([] :: [Int])
+  , testCase "map of non-empty list" $ do
+    [2, 4 .. 8] @=? L.map (+1) [1, 3 .. 7 :: Int]
+  , testCase "filter of empty list" $ do
+    [] @=? L.filter undefined ([] :: [Int])
+  , testCase "filter of normal list" $ do
+    [1, 3] @=? L.filter odd [1 .. 4 :: Int]
+  , testCase "foldl' of empty list" $ do
+    0 @=? L.foldl' (+) (0 :: Int) []
+  , testCase "foldl' of non-empty list" $ do
+    7 @=? L.foldl' (+) (-3) [1 .. 4 :: Int]
+  , testCase "foldl' of huge list" $ do
+    big * (big + 1) `div` 2 @=? L.foldl' (+) 0 [1 .. big]
+  , testCase "foldl' with non-commutative function" $ do
+    0 @=? L.foldl' (-) 10 [1 .. 4 :: Int]
+  , testCase "foldr as id" $ do
+    [1 .. big] @=? L.foldr (:) [] [1 .. big]
+  , testCase "foldr as append" $ do
+    [1 .. big] @=? L.foldr (:) [100 .. big] [1 .. 99]
+  , testCase "++ of empty lists" $ do
+    [] @=? [] L.++ ([] :: [Int])
+  , testCase "++ of empty and non-empty lists" $ do
+    [1 .. 4] @=? [] L.++ [1 .. 4 :: Int]
+  , testCase "++ of non-empty and empty lists" $ do
+    [1 .. 4] @=? [1 .. 4 :: Int] L.++ []
+  , testCase "++ of non-empty lists" $ do
+    [1 .. 5] @=? [1 .. 3] L.++ [4, 5 :: Int]
+  , testCase "++ of large lists" $ do
+    [1 .. big] @=? [1 .. big `div` 2] L.++ [1 + big `div` 2 .. big]
+  , testCase "concat of no lists" $ do
+    [] @=? concat ([] :: [[Int]])
+  , testCase "concat of list of lists" $ do
+    [1 .. 6] @=? concat [[1, 2], [3], [], [4, 5, 6 :: Int]]
+  , testCase "concat of large list of small lists" $ do
+    [1 .. big] @=? concat (map (:[]) [1 .. big])
+  ]
