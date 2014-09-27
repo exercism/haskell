7255f09a6c99fc896cd0afeb9bb68d4e4365a8e4
Bob Ippolito <bob@redivi.com>
2014-09-27 12:29:34 -0700

    do not test hamming distance for strings of inequal length #31

--- point-mutations/point-mutations_test.hs
+++ point-mutations/point-mutations_test.hs
@@ -22,24 +22,8 @@ hammingDistanceTests =
     0 @=? hammingDistance "GGACTGA" "GGACTGA"
   , testCase "complete hamming distance in small strand" $
     3 @=? hammingDistance "ACT" "GGA"
-  , testCase "hamming distance in off by one strand" $
-    19 @=? hammingDistance
-    "GGACGGATTCTGACCTGGACTAATTTTGGGG"
-    "AGGACGGATTCTGACCTGGACTAATTTTGGGG"
   , testCase "small hamming distance in middle somewhere" $
     1 @=? hammingDistance "GGACG" "GGTCG"
   , testCase "larger distance" $
     2 @=? hammingDistance "ACCAGGG" "ACTATGG"
-  , testCase "ignores extra length on other strand when longer" $
-    3 @=? hammingDistance "AAACTAGGGG" "AGGCTAGCGGTAGGAC"
-  , testCase "ignores extra length on original strand when longer" $
-    5 @=? hammingDistance "GACTACGGACAGGGTAGGGAAT" "GACATCGCACACC"
-  , TestLabel "does not actually shorten original strand" $
-    TestList $ map TestCase $
-    [ 1 @=? hammingDistance "AGACAACAGCCAGCCGCCGGATT" "AGGCAA"
-    , 1 @=? hammingDistance "AGACAACAGCCAGCCGCCGGATT" "AGGCAA"
-    , 4 @=? hammingDistance
-      "AGACAACAGCCAGCCGCCGGATT"
-      "AGACATCTTTCAGCCGCCGGATTAGGCAA"
-    , 1 @=? hammingDistance "AGACAACAGCCAGCCGCCGGATT" "AGG" ]
   ]
