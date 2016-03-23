f6913f242eb8ba579a91db8405568f5562946142
Peter Tseng <pht24@cornell.edu>
2016-03-23 06:48:06 -0700

    Revert "DO NOT MERGE: no cache"
    This reverts commit 77dcf3cc88c2f5d3930b306a875fa0fe2ae53715.

--- _test/bootstrap.sh
+++ _test/bootstrap.sh
@@ -8,10 +8,6 @@ fi
 if [ ! -z "$CABALVER" ]; then
     export PATH=/opt/cabal/${CABALVER}/bin:$PATH
 fi
-
-# DO NOT MERGE: Debug what cabal is doing, what's the before and after?
-ghc-pkg list
-
 cabal update
 # This is a fairly minimal set
 cabal install \
