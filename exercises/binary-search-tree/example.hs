052182ba73d6d53bb6b62dd58173200916bb7921
Peter Tseng <pht24@cornell.edu>
2016-04-22 04:20:12 -0700

    binary-search-tree: Use Foldable in example toList
    This doesn't affect the test interface at all, but makes sense in the
    context of #59 - we can now fold over the tree using, say, the monoid of
    lists and concatenation. This means we don't need to explicitly
    implement `toList` but instead use the default implementation from
    Data.Foldable and it will work!

--- exercises/binary-search-tree/example.hs
+++ exercises/binary-search-tree/example.hs
@@ -1,11 +1,27 @@
+{-# LANGUAGE CPP #-}
 module BST ( BST, bstLeft, bstRight, bstValue,
              empty, singleton, insert, fromList, toList
            ) where
-import Data.List (foldl')
+
+#if __GLASGOW_HASKELL__ >= 710
+import Data.Foldable (foldl', toList)
+#else
+-- Foldable, foldMap, mappend, mempty only added to Prelude in 7.10
+import Data.Foldable (Foldable, foldMap, foldl', toList)
+import Data.Monoid (mappend, mempty)
+#endif
 
 data BST a = Tip
            | Node (BST a) a (BST a)
            deriving (Show, Eq)
+
+-- This allows us to use the toList from Foldable.
+-- This may be seen as gratuitous for just one toList function,
+-- but keep in mind now this BST could use other Foldable functions too,
+-- not just toList.
+instance Foldable BST where
+  foldMap _f Tip = mempty
+  foldMap f (Node l v r) = foldMap f l `mappend` f v `mappend` foldMap f r
 
 bstValue :: BST a -> Maybe a
 bstValue Tip = Nothing
@@ -34,7 +50,3 @@ insert x (Node l v r) =
 
 fromList :: Ord a => [a] -> BST a
 fromList = foldl' (flip insert) empty
-
-toList :: BST a -> [a]
-toList Tip = []
-toList (Node l v r) = toList l ++ [v] ++ toList r
