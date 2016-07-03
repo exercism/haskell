d05bc9b9687e98c69bc453c9f6605d6e6c0610df
Peter Tseng <pht24@cornell.edu>
2016-07-02 23:40:20 -0700

    bootstrap: Use dependencies.txt to list packages
    Thanks to #159, we have a list of dependencies of each exercise. We
    should replace the current hard-coded list in `bootstrap.sh` with one
    inferred from `dependencies.txt`.
    
    Closes #175

--- _test/bootstrap.sh
+++ _test/bootstrap.sh
@@ -8,20 +8,15 @@ fi
 if [ ! -z "$CABALVER" ]; then
     export PATH=/opt/cabal/${CABALVER}/bin:$PATH
 fi
-cabal update
-# This is a fairly minimal set
-cabal install \
-      primitive \
-      random \
-      tf-random \
-      HUnit \
-      QuickCheck \
-      split \
-      text \
-      bytestring \
-      attoparsec \
-      vector \
-      parallel \
-      stm \
-      old-locale \
-      lens
+
+# What directory is this script in?
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+deps=$($DIR/dependencies $(ls $DIR/../exercises))
+already_installed=$(ghc-pkg list --names-only --simple-output | tr " " "\n" | sort)
+not_installed=$(comm -23 <(echo "$deps") <(echo "$already_installed"))
+
+if [ -n "$not_installed" ]; then
+  cabal update
+  echo "$not_installed" | xargs cabal install
+fi
