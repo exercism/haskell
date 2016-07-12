93059a4c950de377ddcc72d02f0469ab9c94ad33
rbasso <rbasso@sharpgeeks.net>
2016-07-13 04:46:36 +0900

    etl: Change letter representation to Char
    - Change letter representation from String to Char.
    - Add stub solution.

--- exercises/etl/etl_test.hs
+++ exercises/etl/etl_test.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
 import Test.HUnit (Assertion, (@=?), runTestTT, Test(..), Counts(..))
 import System.Exit (ExitCode(..), exitWith)
 import ETL (transform)
@@ -18,32 +20,32 @@ main = exitProperly $ runTestTT $ TestList
 transformTests :: [Test]
 transformTests =
   [ testCase "transform one value" $
-    M.fromList [("a", 1)] @=? transform (M.fromList [(1, ["A"])])
+    M.fromList [('a', 1)] @=? transform (M.fromList [(1, "A")])
   , testCase "transform multiple keys from one value" $
-    M.fromList [("a", 1), ("e", 1)] @=? transform (M.fromList [(1, ["A", "E"])])
+    M.fromList [('a', 1), ('e', 1)] @=? transform (M.fromList [(1, "AE")])
   , testCase "transform multiple keys from multiple values" $
-    M.fromList [("a", 1), ("b", 4)] @=?
-    transform (M.fromList [(1, ["A"]), (4, ["B"])])
+    M.fromList [('a', 1), ('b', 4)] @=?
+    transform (M.fromList [(1, "A"), (4, "B")])
   , testCase "full dataset" $
     M.fromList fullOut @=? transform (M.fromList fullIn)
   ]
 
-fullOut :: [(String, Int)]
+fullOut :: [(Char, Int)]
 fullOut =
-  [ ("a", 1), ("b", 3), ("c", 3), ("d", 2), ("e", 1)
-  , ("f", 4), ("g", 2), ("h", 4), ("i", 1), ("j", 8)
-  , ("k", 5), ("l", 1), ("m", 3), ("n", 1), ("o", 1)
-  , ("p", 3), ("q", 10), ("r", 1), ("s", 1), ("t", 1)
-  , ("u", 1), ("v", 4), ("w", 4), ("x", 8), ("y", 4)
-  , ("z", 10) ]
+  [ ('a', 1), ('b', 3), ('c', 3), ('d', 2), ('e', 1)
+  , ('f', 4), ('g', 2), ('h', 4), ('i', 1), ('j', 8)
+  , ('k', 5), ('l', 1), ('m', 3), ('n', 1), ('o', 1)
+  , ('p', 3), ('q', 10), ('r', 1), ('s', 1), ('t', 1)
+  , ('u', 1), ('v', 4), ('w', 4), ('x', 8), ('y', 4)
+  , ('z', 10) ]
 
-fullIn :: [(Int, [String])]
+fullIn :: [(Int, String)]
 fullIn =
-  [ (1, map (:[]) "AEIOULNRST")
-  , (2, ["D", "G"])
-  , (3, map (:[]) "BCMP")
-  , (4, map (:[]) "FHVWY")
-  , (5, ["K"])
-  , (8, ["J", "X"])
-  , (10, ["Q", "Z"])
+  [ (1, "AEIOULNRST")
+  , (2, "DG")
+  , (3, "BCMP")
+  , (4, "FHVWY")
+  , (5, "K")
+  , (8, "JX")
+  , (10,"QZ")
   ]
--- exercises/etl/example.hs
+++ exercises/etl/example.hs
@@ -3,9 +3,9 @@ import Data.Char (toLower)
 import qualified Data.Map as M
 
 type PointValue = Int
-type LowerTile = String
-type UpperTile = String
+type LowerTile = Char
+type UpperTile = Char
 
 transform :: M.Map PointValue [UpperTile] -> M.Map LowerTile PointValue
 transform = M.fromList . concatMap go . M.toList
-  where go (v, tiles) = zip (map (map toLower) tiles) (repeat v)
+  where go (v, tiles) = zip (map toLower tiles) (repeat v)
