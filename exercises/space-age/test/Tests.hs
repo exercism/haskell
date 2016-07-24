feae65265d67e4d15e9fe4cbc8deda2c2c21e182
rbasso <rbasso@sharpgeeks.net>
2016-07-25 03:56:53 +0900

    space-age: Rewrite tests to use hspec with fail-fast.
    - Rewrite to use hspec.
    - Replace old tests by tests matching the json file.

--- exercises/space-age/test/Tests.hs
+++ exercises/space-age/test/Tests.hs
@@ -1,54 +1,73 @@
-import Test.HUnit (Assertion, (@=?), runTestTT, Test(..), Counts(..))
-import System.Exit (ExitCode(..), exitWith)
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+{-# LANGUAGE RecordWildCards #-}
+
+import Data.Foldable     (for_)
+import Test.Hspec        (Spec, describe, it, shouldBe)
+import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+
 import SpaceAge (Planet(..), ageOn)
 
-exitProperly :: IO Counts -> IO ()
-exitProperly m = do
-  counts <- m
-  exitWith $ if failures counts /= 0 || errors counts /= 0 then ExitFailure 1 else ExitSuccess
-
-testCase :: String -> Assertion -> Test
-testCase label assertion = TestLabel label (TestCase assertion)
-
 main :: IO ()
-main = exitProperly $ runTestTT $ TestList
-       [ TestList ageOnTests ]
+main = hspecWith defaultConfig {configFastFail = True} specs
 
-roundsTo :: Double -> Double -> Assertion
-roundsTo a b = approx a @=? approx b
-  where approx :: Double -> Int
-        approx n = round (n * 100)
+specs :: Spec
+specs = describe "space-age" $
+          describe "ageOn" $ for_ cases test
+  where
 
-ageOnTests :: [Test]
-ageOnTests =
-  [ testCase "age in earth years" $
-    31.69 `roundsTo` ageOn Earth 1000000000
-  , testCase "age in mercury years" $ do
-    let seconds = 2134835688
-    67.65 `roundsTo` ageOn Earth seconds
-    280.88 `roundsTo` ageOn Mercury seconds
-  , testCase "age in venus years" $ do
-    let seconds = 189839836
-    6.02 `roundsTo` ageOn Earth seconds
-    9.78 `roundsTo` ageOn Venus seconds
-  , testCase "age on mars" $ do
-    let seconds = 2329871239
-    73.83 `roundsTo` ageOn Earth seconds
-    39.25 `roundsTo` ageOn Mars seconds
-  , testCase "age on jupiter" $ do
-    let seconds = 901876382
-    28.58 `roundsTo` ageOn Earth seconds
-    2.41 `roundsTo` ageOn Jupiter seconds
-  , testCase "age on saturn" $ do
-    let seconds = 3000000000
-    95.06 `roundsTo` ageOn Earth seconds
-    3.23 `roundsTo` ageOn Saturn seconds
-  , testCase "age on uranus" $ do
-    let seconds = 3210123456
-    101.72 `roundsTo` ageOn Earth seconds
-    1.21 `roundsTo` ageOn Uranus seconds
-  , testCase "age on neptune" $ do
-    let seconds = 8210123456
-    260.16 `roundsTo` ageOn Earth seconds
-    1.58 `roundsTo` ageOn Neptune seconds
-  ]
+    test Case{..} = it description assertion
+      where
+        expression  = ageOn planet $ fromIntegral seconds
+        assertion   = roundAge expression `shouldBe` roundAge expected
+        roundAge    = (/ 100) . fromIntegral . round . (* 100)
+
+-- Test cases adapted from `exercism/x-common/space-age.json` on 2016-07-27.
+
+data Case = Case { description :: String
+                 , planet      :: Planet
+                 , seconds     :: Integer
+                 , expected    :: Double
+                 }
+
+cases :: [Case]
+cases = [ Case { description = "Earth"
+               , planet      = Earth
+               , seconds     = 1000000000
+               , expected    = 31.69
+               }
+        , Case { description = "Mercury"
+               , planet      = Mercury
+               , seconds     = 2134835688
+               , expected    = 280.88
+               }
+        , Case { description = "Venus"
+               , planet      = Venus
+               , seconds     = 189839836
+               , expected    = 9.78
+               }
+        , Case { description = "Mars"
+               , planet      = Mars
+               , seconds     = 2329871239
+               , expected    = 39.25
+               }
+        , Case { description = "Jupiter"
+               , planet      = Jupiter
+               , seconds     = 901876382
+               , expected    = 2.41
+               }
+        , Case { description = "Saturn"
+               , planet      = Saturn
+               , seconds     = 3000000000
+               , expected    = 3.23
+               }
+        , Case { description = "Uranus"
+               , planet      = Uranus
+               , seconds     = 3210123456
+               , expected    = 1.21
+               }
+        , Case { description = "Neptune"
+               , planet      = Neptune
+               , seconds     = 8210123456
+               , expected    = 1.58
+               }
+        ]
