fe00df0e0aa3f3b72e52d9b6bcd5c76a1abf15ea
rbasso <rbasso@sharpgeeks.net>
2016-07-27 01:01:14 +0900

    triangle: Rewrite test to use hspec with fail-fast.

--- exercises/triangle/test/Tests.hs
+++ exercises/triangle/test/Tests.hs
@@ -1,28 +1,33 @@
-import Test.HUnit ((@=?), runTestTT, Test(..), Counts(..))
-import System.Exit (ExitCode(..), exitWith)
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+import Data.Foldable     (for_)
+import Test.Hspec        (Spec, describe, it, shouldBe)
+import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+
 import Triangle (TriangleType(..), triangleType)
 
-exitProperly :: IO Counts -> IO ()
-exitProperly m = do
-  counts <- m
-  exitWith $ if failures counts /= 0 || errors counts /= 0 then ExitFailure 1 else ExitSuccess
-
 main :: IO ()
-main = exitProperly $ runTestTT $ TestList
-       [ TestList triangleTests ]
+main = hspecWith defaultConfig {configFastFail = True} specs
 
-tri :: Int -> Int -> Int -> TriangleType
-tri = triangleType
+specs :: Spec
+specs = describe "triangle" $
+          describe "triangleType" $ for_ cases test
+  where
 
-triangleTests :: [Test]
-triangleTests = map TestCase
-  [ Equilateral @=? tri 2 2 2
-  , Equilateral @=? tri 10 10 10
-  , Isosceles @=? tri 3 4 4
-  , Isosceles @=? tri 4 3 4
-  , Isosceles @=? tri 9 12 9
-  , Scalene @=? tri 3 4 5
-  , Illogical @=? tri 1 1 50
-  , Illogical @=? tri 1 2 1
-  , Illogical @=? tri 0 0 0
-  ]
+    test (a, b, c, expected) = it explanation assertion
+      where
+        explanation = unwords . map show $ [a, b, c]
+        assertion   = triangleType a b c  `shouldBe` expected
+
+    -- As of 2016-07-30, there was no reference file
+    -- for the test cases in `exercism/x-common`.
+
+    cases = [ ( 2,  2,  2, Equilateral)
+            , (10, 10, 10, Equilateral)
+            , ( 3,  4,  4, Isosceles  )
+            , ( 4,  3,  4, Isosceles  )
+            , ( 9, 12,  9, Isosceles  )
+            , ( 3,  4,  5, Scalene    )
+            , ( 1,  1, 50, Illogical  )
+            , ( 1,  2,  1, Illogical  )
+            , ( 0,  0,  0, Illogical  ) ]
