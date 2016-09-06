36d75c1b9c4044075513b61179de38bc0e5d6468
rbasso <rbasso@sharpgeeks.net>
2016-09-06 15:58:59 +0900

    palindrome-products: Rewrite tests to use hspec with fail-fast.
    - Rewrite tests to use `hspec`.
    - Add `HINTS.md`.
    - Move old hints to `HINTS.md`.
    - Update the stub solution.

--- exercises/palindrome-products/src/Palindromes.hs
+++ exercises/palindrome-products/src/Palindromes.hs
@@ -1,15 +1,7 @@
 module Palindromes (largestPalindrome, smallestPalindrome) where
 
--- It's ok to return duplicates in the factor list, and the order of the factors
--- is irrelevant.
---
--- You should consider using a slightly different algorithm to find small or
--- large palindromes.
+largestPalindrome :: Integer -> Integer -> (Integer, [(Integer, Integer)])
+largestPalindrome minFactor maxFactor = undefined
 
--- largestPalindrome minFactor maxFactor = (value, [(factor1, factor2)])
-largestPalindrome :: Integral a => a -> a -> (a, [(a, a)])
-largestPalindrome = undefined
-
--- smallestPalindrome minFactor maxFactor = (value, [(factor1, factor2)])
-smallestPalindrome :: Integral a => a -> a -> (a, [(a, a)])
-smallestPalindrome = undefined
+smallestPalindrome :: Integer -> Integer -> (Integer, [(Integer, Integer)])
+smallestPalindrome minFactor maxFactor = undefined
