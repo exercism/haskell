2771e698a7b4fc0447868a94f058a92338ed0108
Peter Tseng <pht24@cornell.edu>
2016-09-13 08:04:28 -0700

    series: Test series of zero
    This seems a useful case to test. There is one series of size zero in
    all series.
    
    The solution does need to be changed here, otherwise it would have said
    that `series 0 "012"` was `[[],[],[]]` instead of `[[]]`.

--- exercises/series/src/Example.hs
+++ exercises/series/src/Example.hs
@@ -4,5 +4,6 @@ import Data.Char (digitToInt)
 import Data.List (tails)
 
 slices :: Int -> String -> [[Int]]
+slices 0 _ = [[]]
 slices n s = map (take n) . take (length s - n + 1) . tails $ numberSeries
     where numberSeries = map digitToInt s
