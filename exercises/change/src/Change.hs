eb53ada3d9b7d4fc799e5e6c5952f0bc1e55543c
abo64 <abo64@gmx.de>
2016-09-19 12:08:15 +0200

    change: Add new exercise. (#306)
    Adds new exercise `change`.
    
    * added change to config.json
    
    * moved change exercise before connect
    
    * code formatting, fixed comment
    
    * fixed missing closing bracket
    
    * removed unused dependency
    
    * fixed shadowing
    
    * applied eta-reduction

---
+++ exercises/change/src/Change.hs
@@ -0,0 +1,4 @@
+module Change (findFewestCoins) where
+
+findFewestCoins :: Integer -> [Integer] -> Maybe [Integer]
+findFewestCoins target coins = undefined
---
+++ exercises/change/src/Example.hs
@@ -0,0 +1,29 @@
+module Change (findFewestCoins) where
+
+import Data.List (sortBy)
+
+type Coin = Integer
+type Coins = [Coin]
+type Amount = Integer
+
+findFewestCoins :: Amount -> Coins -> Maybe Coins
+findFewestCoins target coins = minChange target sortedCoins [] Nothing
+  where
+    sortedCoins = sortBy (flip compare) coins
+
+    minChange target' coins' candidate bestResult
+      | target' < 0 || worseResult = bestResult
+      | target' == 0 = Just candidate
+      | otherwise = dropCoin target' coins' candidate newBestResult
+      where
+        worseResult = maybe False (\x -> length x <= length candidate) bestResult
+        newBestResult = addCoin target' coins' candidate bestResult
+
+    addCoin target' coins'@(coin:_) candidate
+      | newTarget >= 0 = minChange newTarget coins' (coin:candidate)
+      where newTarget = target' - coin
+    addCoin _ _ _ = id
+
+    dropCoin target' (_:restCoins) = minChange target' restCoins
+    dropCoin _ _ = flip const
+
