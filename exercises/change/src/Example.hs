56736e40be9fb6a6df70a91d1929f52552425c53
Achim Botz <achim.botz@teradata.com>
2016-09-19 10:55:50 +0200

    fixed shadowing

--- exercises/change/src/Example.hs
+++ exercises/change/src/Example.hs
@@ -11,19 +11,20 @@ findFewestCoins target coins = minChange target sortedCoins [] Nothing
   where
     sortedCoins = sortBy (flip compare) coins
 
-    minChange target coins candidate bestResult
-      | target < 0 || worseResult = bestResult
-      | target == 0 = Just candidate
-      | otherwise = dropCoin target coins candidate newBestResult
+    minChange target' coins' candidate bestResult
+      | target' < 0 || worseResult = bestResult
+      | target' == 0 = Just candidate
+      | otherwise = dropCoin target' coins' candidate newBestResult
       where
         worseResult = maybe False (\x -> length x <= length candidate) bestResult
-        newBestResult = addCoin target coins candidate bestResult
+        newBestResult = addCoin target' coins' candidate bestResult
 
-    addCoin target coins@(coin:_) candidate bestResult
-      | newTarget >= 0 = minChange newTarget coins (coin:candidate) bestResult
-      where newTarget = target - coin
+    addCoin target' coins'@(coin:_) candidate bestResult
+      | newTarget >= 0 = minChange newTarget coins' (coin:candidate) bestResult
+      where newTarget = target' - coin
     addCoin _ _ _ bestResult = bestResult
 
-    dropCoin target (_:restCoins) candidate bestResult =
-      minChange target restCoins candidate bestResult
+    dropCoin target' (_:restCoins) candidate bestResult =
+      minChange target' restCoins candidate bestResult
     dropCoin _ _ _ bestResult = bestResult
+
