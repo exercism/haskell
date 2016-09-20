cfe8daa4140bedf18b77009d74699fdf374bd54c
rbasso <rbasso@sharpgeeks.net>
2016-09-20 18:33:54 +0900

    grade-school: Move hints to HINTS.md

---
+++ exercises/grade-school/HINTS.md
@@ -0,0 +1,9 @@
+## Hints
+
+To complete this exercise you need to create the data type `School`
+and implement the following functions:
+
+- `add`
+- `empty`
+- `grade`
+- `sorted`
--- exercises/grade-school/src/School.hs
+++ exercises/grade-school/src/School.hs
@@ -1,8 +1,5 @@
 module School (School, add, empty, grade, sorted) where
 
--- The task is to create the data type `School` and
--- implement the functions below.
-
 add = undefined
 
 empty = undefined
