239b792e60095bd6e2c5857bfc6b7c9528180f09
rbasso <rbasso@sharpgeeks.net>
2016-09-20 23:02:15 +0900

    meetup: Move hints to HINTS.md

---
+++ exercises/meetup/HINTS.md
@@ -0,0 +1,7 @@
+## Hints
+
+To complete this exercise, you need to create the data types `Weekday`
+and `Schedule`, and implement the function `meetupDay`.
+
+You will find the type signature for `meetupDay` already in place,
+but it is up to you to define the function.
--- exercises/meetup/src/Meetup.hs
+++ exercises/meetup/src/Meetup.hs
@@ -2,8 +2,5 @@ module Meetup (Weekday(..), Schedule(..), meetupDay) where
 
 import Data.Time.Calendar (Day)
 
--- The task is to create the data types `Weekday` and
--- `Schedule`, and implement the function `meetupDay`.
-
 meetupDay :: Schedule -> Weekday -> Integer -> Int -> Day
 meetupDay schedule weekday year month = undefined
