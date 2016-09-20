6cdb26f6781a176b1e47a2e6a3a81aea8b6349d9
rbasso <rbasso@sharpgeeks.net>
2016-09-20 23:53:09 +0900

    space-age: Move hints to HINTS.md

---
+++ exercises/space-age/HINTS.md
@@ -0,0 +1,4 @@
+## Hints
+
+To complete this exercise, you need to create the data type `Planet`
+and implement the `ageOn` function.
--- exercises/space-age/src/SpaceAge.hs
+++ exercises/space-age/src/SpaceAge.hs
@@ -1,6 +1,3 @@
 module SpaceAge (Planet(..), ageOn) where
 
--- The task is to create the data type `Planet`
--- and implement the function `ageOn`.
-
 ageOn = undefined
