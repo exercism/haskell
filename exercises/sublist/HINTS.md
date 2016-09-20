64d4cff399a072672841fcfc72b4fca288a67561
rbasso <rbasso@sharpgeeks.net>
2016-09-21 00:11:06 +0900

    sublist: Move hints to HINTS.md

---
+++ exercises/sublist/HINTS.md
@@ -0,0 +1,4 @@
+## Hints
+
+To complete this exercise, you need to create the data type `Sublist`,
+with `Eq` and `Show` instances, and implement the `sublist` function.
--- exercises/sublist/src/Sublist.hs
+++ exercises/sublist/src/Sublist.hs
@@ -1,6 +1,3 @@
 module Sublist (Sublist(..), sublist) where
 
--- The task is to create the data type `Sublist`, with `Eq` and
--- `Show` instances, and implement the function `sublist`.
-
 sublist = undefined
