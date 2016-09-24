87289e661ee17dc2b0d6270dddfae3dfc2dce473
rbasso <rbasso@sharpgeeks.net>
2016-09-25 03:26:02 +0900

    raindrops: Improve code based on HLint's suggestions.

--- exercises/raindrops/src/Example.hs
+++ exercises/raindrops/src/Example.hs
@@ -1,7 +1,9 @@
 module Raindrops (convert) where
 
+import Data.Maybe (fromMaybe)
+
 convert :: Int -> String
-convert n = maybe (show n) id maybeSound
+convert n = fromMaybe (show n) maybeSound
   where
     maybeSound = sound "Pling" 3 `mappend`
                  sound "Plang" 5 `mappend`
