e04dcb782b5335447cb676ebe7e4541cdccdaa38
rbasso <rbasso@sharpgeeks.net>
2016-09-25 03:04:32 +0900

    hamming: Improve code based on HLint's suggestions.

--- exercises/hamming/src/Example.hs
+++ exercises/hamming/src/Example.hs
@@ -1,6 +1,6 @@
 module Hamming (distance) where
 
 distance :: Integral a => String -> String -> Maybe a
-distance (x:xs) (y:ys) = fmap (if x /= y then (+1) else id) $ distance xs ys
+distance (x:xs) (y:ys) = (if x /= y then (+1) else id) <$> distance xs ys
 distance    []     []  = Just 0
 distance     _      _  = Nothing
