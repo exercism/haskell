93de6e71dc6d847368e3780e455e4105932dd920
rbasso <rbasso@sharpgeeks.net>
2016-09-27 17:15:43 +0900

    trinary: Move hints to HINTS.md

--- exercises/trinary/test/Tests.hs
+++ exercises/trinary/test/Tests.hs
@@ -7,19 +7,6 @@ import qualified Numeric as Num (showIntAtBase)
 
 import Trinary (readTri, showTri)
 
-{-
-For the appropriate amount of challenge here, you should only
-use functionality present in Prelude. Try not to use
-Data.List, Data.Char, Data.Bits, or Numeric.
-
-Try and use seq, $!, or BangPatterns appropriately to ensure
-that the solution is efficient.
-
-Handling invalid input is not necessary.
-
-If you've done the Octal exercise, perhaps you should generalize it.
--}
-
 main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
