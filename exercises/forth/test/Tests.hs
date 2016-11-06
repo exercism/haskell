004ca7f5427aef70fa732fb77ce4774da2335c70
Peter Tseng <pht24@cornell.edu>
2016-11-05 19:25:59 -0700

    forth: Break up tests into categories
    Just as https://github.com/exercism/x-common/pull/394 has it.
    
    Most test lines should look clean under `git diff -w` ignoring
    whitespace since they were simply indented.
    
    EXCEPTIONS:
    
    swap/over tests have changed ordering: The "nothing on stack" tests now
    come before the "one value on the stack" tests.
    
    The divide by zero test is now a simple `4 0 /` rather than `4 2 2 - /`
    to be more straightforward.
    It is separated from the combined arithmetic tests, in anticipate of
    more dedicated division tests coming soon.

--- exercises/forth/test/Tests.hs
+++ exercises/forth/test/Tests.hs
@@ -17,62 +17,82 @@ specs = describe "forth" $ do
 
     let runTexts = fmap toList . foldM (flip evalText) empty
 
-    it "no input, no stack" $
-      toList empty `shouldBe` []
+    describe "parsing and numbers" $ do
+      it "empty input results in empty stack" $
+        toList empty `shouldBe` []
 
-    it "numbers just get pushed onto the stack" $
-      runTexts ["1 2 3 4 5"] `shouldBe` Right [1, 2, 3, 4, 5]
+      it "numbers just get pushed onto the stack" $
+        runTexts ["1 2 3 4 5"] `shouldBe` Right [1, 2, 3, 4, 5]
 
-    it "non-word characters are separators" $
-      runTexts ["1\NUL2\SOH3\n4\r5 6\t7"] `shouldBe` Right [1, 2, 3, 4, 5, 6, 7]
+      it "all non-word characters are separators" $
+        runTexts ["1\NUL2\SOH3\n4\r5 6\t7"] `shouldBe` Right [1, 2, 3, 4, 5, 6, 7]
 
-    it "basic arithmetic" $ do
-      runTexts ["1 2 + 4 -"] `shouldBe` Right [-1]
-      runTexts ["2 4 * 3 /"] `shouldBe` Right [2]
+    describe "division" $
+      it "errors if dividing by zero" $
+        runTexts ["4 0 /"] `shouldBe` Left DivisionByZero
 
-    it "division by zero" $
-      runTexts ["4 2 2 - /"] `shouldBe` Left DivisionByZero
+    describe "combined arithmetic" $ do
+      it "addition and subtraction" $
+        runTexts ["1 2 + 4 -"] `shouldBe` Right [-1]
 
-    it "dup" $ do
-      runTexts ["1 DUP"  ] `shouldBe` Right [1, 1]
-      runTexts ["1 2 Dup"] `shouldBe` Right [1, 2, 2]
-      runTexts ["dup"    ] `shouldBe` Left StackUnderflow
+      it "multiplication and division" $
+        runTexts ["2 4 * 3 /"] `shouldBe` Right [2]
 
-    it "drop" $ do
-      runTexts ["1 drop"  ] `shouldBe` Right []
-      runTexts ["1 2 drop"] `shouldBe` Right [1]
-      runTexts ["drop"    ] `shouldBe` Left StackUnderflow
+    describe "dup" $ do
+      it "copies the top value on the stack" $
+        runTexts ["1 DUP"  ] `shouldBe` Right [1, 1]
+      it "is case-insensitive" $
+        runTexts ["1 2 Dup"] `shouldBe` Right [1, 2, 2]
+      it "errors if there is nothing on the stack" $
+        runTexts ["dup"    ] `shouldBe` Left StackUnderflow
 
-    it "swap" $ do
-      runTexts ["1 2 swap"  ] `shouldBe` Right [2, 1]
-      runTexts ["1 2 3 swap"] `shouldBe` Right [1, 3, 2]
-      runTexts ["1 swap"    ] `shouldBe` Left StackUnderflow
-      runTexts ["swap"      ] `shouldBe` Left StackUnderflow
+    describe "drop" $ do
+      it "removes the top value on the stack if it is the only one" $
+        runTexts ["1 drop"  ] `shouldBe` Right []
+      it "removes the top value on the stack if it is not the only one" $
+        runTexts ["1 2 drop"] `shouldBe` Right [1]
+      it "errors if there is nothing on the stack" $
+        runTexts ["drop"    ] `shouldBe` Left StackUnderflow
 
-    it "over" $ do
-      runTexts ["1 2 over"  ] `shouldBe` Right [1, 2, 1]
-      runTexts ["1 2 3 over"] `shouldBe` Right [1, 2, 3, 2]
-      runTexts ["1 over"    ] `shouldBe` Left StackUnderflow
-      runTexts ["over"      ] `shouldBe` Left StackUnderflow
+    describe "swap" $ do
+      it "swaps the top two values on the stack if they are the only ones" $
+        runTexts ["1 2 swap"  ] `shouldBe` Right [2, 1]
+      it "swaps the top two values on the stack if they are not the only ones" $
+        runTexts ["1 2 3 swap"] `shouldBe` Right [1, 3, 2]
+      it "errors if there is nothing on the stack" $
+        runTexts ["swap"      ] `shouldBe` Left StackUnderflow
+      it "errors if there is only one value on the stack" $
+        runTexts ["1 swap"    ] `shouldBe` Left StackUnderflow
 
-    it "defining a new word" $
-      runTexts [ ": dup-twice dup dup ;"
-               , "1 dup-twice"           ] `shouldBe` Right [1, 1, 1]
+    describe "over" $ do
+      it "copies the second element if there are only two" $
+        runTexts ["1 2 over"  ] `shouldBe` Right [1, 2, 1]
+      it "copies the second element if there are more than two" $
+        runTexts ["1 2 3 over"] `shouldBe` Right [1, 2, 3, 2]
+      it "errors if there is nothing on the stack" $
+        runTexts ["over"      ] `shouldBe` Left StackUnderflow
+      it "errors if there is only one value on the stack" $
+        runTexts ["1 over"    ] `shouldBe` Left StackUnderflow
 
-    it "redefining an existing word" $
-      runTexts [ ": foo dup ;"
-               , ": foo dup dup ;"
-               , "1 foo"           ] `shouldBe` Right [1, 1, 1]
+    describe "user-defined words" $ do
+      it "can consist of built-in words" $
+        runTexts [ ": dup-twice dup dup ;"
+                 , "1 dup-twice"           ] `shouldBe` Right [1, 1, 1]
 
-    it "redefining an existing built-in word" $
-      runTexts [ ": swap dup ;"
-               , "1 swap"       ] `shouldBe` Right [1, 1]
+      it "can override other user-defined words" $
+        runTexts [ ": foo dup ;"
+                 , ": foo dup dup ;"
+                 , "1 foo"           ] `shouldBe` Right [1, 1, 1]
 
-    it "defining words with odd characters" $
-      runTexts [": € 220371 ; €"] `shouldBe` Right [220371]
+      it "can override built-in words" $
+        runTexts [ ": swap dup ;"
+                 , "1 swap"       ] `shouldBe` Right [1, 1]
 
-    it "defining a number" $
-      runTexts [": 1 2 ;"] `shouldBe` Left InvalidWord
+      it "can consist of arbitrary characters such as Unicode characters" $
+        runTexts [": € 220371 ; €"] `shouldBe` Right [220371]
 
-    it "calling a non-existing word" $
-      runTexts ["1 foo"] `shouldBe` Left (UnknownWord "foo")
+      it "cannot redefine numbers" $
+        runTexts [": 1 2 ;"] `shouldBe` Left InvalidWord
+
+      it "errors if executing a non-existent word" $
+        runTexts ["1 foo"] `shouldBe` Left (UnknownWord "foo")
