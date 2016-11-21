2f165513b7eed9e6e97edb1102a2b539fb3942c6
abo64 <abo64@gmx.de>
2016-11-21 08:48:28 +0100

    new exercise bowling (#439)
    * new exercise bowling
    
    * use Dummy data type, added HINTS.md

---
+++ exercises/bowling/src/Bowling.hs
@@ -0,0 +1,13 @@
+module Bowling (bowlingStart, roll, score) where
+
+data Bowling = Dummy
+
+bowlingStart :: Bowling
+bowlingStart = undefined
+
+roll :: Bowling -> Int -> Bowling
+roll = undefined
+
+score :: Bowling -> Maybe Int
+score = undefined
+
