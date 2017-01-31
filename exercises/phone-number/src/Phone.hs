60bc990eb0a05dcce98b2055872333c19a7ad8a6
Peter Tseng <petertseng@users.noreply.github.com>
2017-01-31 12:26:13 -0800

    phone-number: simplify to number only (#480)
    areaCode and prettyPrint are hereby removed.
    
    https://github.com/exercism/x-common/pull/477

--- exercises/phone-number/src/Phone.hs
+++ exercises/phone-number/src/Phone.hs
@@ -1,10 +1,4 @@
-module Phone (areaCode, number, prettyPrint) where
-
-areaCode :: String -> Maybe String
-areaCode = error "You need to implement this function."
+module Phone (number) where
 
 number :: String -> Maybe String
 number = error "You need to implement this function."
-
-prettyPrint :: String -> Maybe String
-prettyPrint = error "You need to implement this function."
