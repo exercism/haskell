46c0966c11dcbfc2535a49468ace6c62b816a86f
Peter Tseng <pht24@cornell.edu>
2017-01-31 03:55:18 -0800

    triangle: Mark divergence from x-common
    If we followed https://github.com/exercism/x-common/pull/424 then we
    would check boolean properties for triangles formed from the inputs. We
    argue that this is unidiomatic in this language, so we are staying with
    the old version of the tests. We mark the divergence as of this day, so
    that the script will recognise this decision as having been made after
    the x-common change.

--- exercises/triangle/test/Tests.hs
+++ exercises/triangle/test/Tests.hs
@@ -25,7 +25,8 @@ specs = describe "triangle" $
       where
         assertion = triangleType a b c `shouldBe` expected
 
-    -- Test cases adapted from `exercism/x-common/triangle.json` on 2016-08-03.
+    -- Test cases created from old version of `x-common` tests on 2017-01-31,
+    -- diverging from `exercism/x-common/triangle`.
 
     cases = [ ( "equilateral triangle has all sides equal"
               , (2, 2, 2)
