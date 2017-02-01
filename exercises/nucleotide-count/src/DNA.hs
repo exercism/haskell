24fd7659922909484d3050c9a1904dfeafe20dc0
Peter Tseng <petertseng@users.noreply.github.com>
2017-02-01 10:15:10 -0800

    nucleotide-count: simplify to only nucleotideCounts (#482)
    It was decided that the exercise was doing too many things, and count
    really is just one element of nucleotideCounts anyway.
    
    https://github.com/exercism/x-common/pull/505

--- exercises/nucleotide-count/src/DNA.hs
+++ exercises/nucleotide-count/src/DNA.hs
@@ -1,9 +1,6 @@
-module DNA (count, nucleotideCounts) where
+module DNA (nucleotideCounts) where
 
 import Data.Map (Map)
-
-count :: Char -> String -> Either String Int
-count = error "You need to implement this function."
 
 nucleotideCounts :: String -> Either String (Map Char Int)
 nucleotideCounts = error "You need to implement this function."
