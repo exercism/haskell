b465c3319b9225ab82b219d3d6c3cecf58a1f59e
mttakai <marjorie.takai@gmail.com>
2017-02-14 21:15:43 +0900

    Accumulate exercise: variable added

--- exercises/accumulate/src/Accumulate.hs
+++ exercises/accumulate/src/Accumulate.hs
@@ -1,4 +1,4 @@
 module Accumulate (accumulate) where
 
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate = error "You need to implement this function."
+accumulate f xs = error "You need to implement this function."
