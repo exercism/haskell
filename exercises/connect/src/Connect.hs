78ea086e45ea42ebb9f78a28255f286a7e330f7d
Luciano Palma <lucianopalma90@gmail.com>
2017-03-11 18:44:51 +0000

    connect: add explicit args

--- exercises/connect/src/Connect.hs
+++ exercises/connect/src/Connect.hs
@@ -3,4 +3,4 @@ module Connect (Mark(..), winner) where
 data Mark = Cross | Nought deriving (Eq, Show)
 
 winner :: [String] -> Maybe Mark
-winner = error "You need to implement this function."
+winner board = error "You need to implement this function."
