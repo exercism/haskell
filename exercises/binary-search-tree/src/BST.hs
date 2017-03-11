799e8244570194511b74ff0e8109a25c1d6645b2
Luciano Palma <lucianopalma90@gmail.com>
2017-03-11 18:37:42 +0000

    binary-search-tree: add explicit args

--- exercises/binary-search-tree/src/BST.hs
+++ exercises/binary-search-tree/src/BST.hs
@@ -13,25 +13,25 @@ module BST
 data BST a = Dummy deriving (Eq, Show)
 
 bstLeft :: BST a -> Maybe (BST a)
-bstLeft = error "You need to implement this function."
+bstLeft tree = error "You need to implement this function."
 
 bstRight :: BST a -> Maybe (BST a)
-bstRight = error "You need to implement this function."
+bstRight tree = error "You need to implement this function."
 
 bstValue :: BST a -> Maybe a
-bstValue = error "You need to implement this function."
+bstValue tree = error "You need to implement this function."
 
 empty :: BST a
 empty = error "You need to implement this function."
 
 fromList :: Ord a => [a] -> BST a
-fromList = error "You need to implement this function."
+fromList xs = error "You need to implement this function."
 
 insert :: Ord a => a -> BST a -> BST a
-insert = error "You need to implement this function."
+insert x tree = error "You need to implement this function."
 
 singleton :: a -> BST a
-singleton = error "You need to implement this function."
+singleton x = error "You need to implement this function."
 
 toList :: BST a -> [a]
-toList = error "You need to implement this function."
+toList tree = error "You need to implement this function."
