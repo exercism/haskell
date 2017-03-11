30cec3798f2758e82521ae5299e5aa4bc2833808
Luciano Palma <lucianopalma90@gmail.com>
2017-03-11 21:04:13 +0000

    largest-series-product: add explicit args

--- exercises/largest-series-product/src/Series.hs
+++ exercises/largest-series-product/src/Series.hs
@@ -1,4 +1,4 @@
 module Series (largestProduct) where
 
 largestProduct :: Int -> String -> Maybe Integer
-largestProduct = error "You need to implement this function."
+largestProduct series digits = error "You need to implement this function."
