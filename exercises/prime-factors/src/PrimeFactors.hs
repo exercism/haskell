401c72cfbe7f7dc2860693a76c94560433010909
Luciano Palma <lucianopalma90@gmail.com>
2017-03-14 17:41:44 +0000

    prime-factors: add explicit args

--- exercises/prime-factors/src/PrimeFactors.hs
+++ exercises/prime-factors/src/PrimeFactors.hs
@@ -1,4 +1,4 @@
 module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
-primeFactors = error "You need to implement this function."
+primeFactors n = error "You need to implement this function."
