544f0b25a37ad59f72d9e6ddc3c5f807f8046331
Luciano Palma <lucianopalma90@gmail.com>
2017-03-17 21:00:43 +0000

    robot-simulator: add explicit args

--- exercises/robot-simulator/src/Robot.hs
+++ exercises/robot-simulator/src/Robot.hs
@@ -17,19 +17,19 @@ data Bearing = North
 data Robot = Dummy
 
 bearing :: Robot -> Bearing
-bearing = error "You need to implement this function."
+bearing robot = error "You need to implement this function."
 
 coordinates :: Robot -> (Integer, Integer)
-coordinates = error "You need to implement this function."
+coordinates robot = error "You need to implement this function."
 
 mkRobot :: Bearing -> (Integer, Integer) -> Robot
-mkRobot = error "You need to implement this function."
+mkRobot direction coordinates = error "You need to implement this function."
 
 simulate :: Robot -> String -> Robot
-simulate = error "You need to implement this function."
+simulate robot instructions = error "You need to implement this function."
 
 turnLeft :: Bearing -> Bearing
-turnLeft = error "You need to implement this function."
+turnLeft direction = error "You need to implement this function."
 
 turnRight :: Bearing -> Bearing
-turnRight = error "You need to implement this function."
+turnRight direction = error "You need to implement this function."
