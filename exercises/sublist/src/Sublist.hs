721d139295a3bb1ecc682a8a2bc8b14fc261d93d
Luciano Palma <lucianopalma90@gmail.com>
2017-03-19 15:22:23 +0000

    sublist: add explicit args

--- exercises/sublist/src/Sublist.hs
+++ exercises/sublist/src/Sublist.hs
@@ -3,4 +3,4 @@ module Sublist (Sublist(..), sublist) where
 data Sublist = Equal | Sublist | Superlist | Unequal deriving (Eq, Show)
 
 sublist :: [a] -> [a] -> Sublist
-sublist = error "You need to implement this function."
+sublist xs ys = error "You need to implement this function."
