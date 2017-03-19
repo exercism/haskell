c0f3626283d112e1416eab8061cb28f5e8e43328
Luciano Palma <lucianopalma90@gmail.com>
2017-03-19 15:19:56 +0000

    simple-linked-list: add explicit args

--- exercises/simple-linked-list/src/LinkedList.hs
+++ exercises/simple-linked-list/src/LinkedList.hs
@@ -13,25 +13,25 @@ module LinkedList
 data LinkedList a = Dummy
 
 datum :: LinkedList a -> a
-datum = error "You need to implement this function."
+datum linkedList = error "You need to implement this function."
 
 fromList :: [a] -> LinkedList a
-fromList = error "You need to implement this function."
+fromList xs = error "You need to implement this function."
 
 isNil :: LinkedList a -> Bool
-isNil = error "You need to implement this function."
+isNil linkedList = error "You need to implement this function."
 
 new :: a -> LinkedList a -> LinkedList a
-new = error "You need to implement this function."
+new x linkedList = error "You need to implement this function."
 
 next :: LinkedList a -> LinkedList a
-next = error "You need to implement this function."
+next linkedList = error "You need to implement this function."
 
 nil :: LinkedList a
 nil = error "You need to implement this function."
 
 reverseLinkedList :: LinkedList a -> LinkedList a
-reverseLinkedList = error "You need to implement this function."
+reverseLinkedList linkedList = error "You need to implement this function."
 
 toList :: LinkedList a -> [a]
-toList = error "You need to implement this function."
+toList linkedList = error "You need to implement this function."
