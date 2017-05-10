23611dcbca79b0246c273034b1b5646bc99fb0da
rbasso <rbasso@sharpgeeks.net>
2017-05-10 14:49:09 +0900

    rna-transcription: Remove test group with exercise's name

--- exercises/rna-transcription/test/Tests.hs
+++ exercises/rna-transcription/test/Tests.hs
@@ -10,8 +10,7 @@ main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
-specs = describe "rna-transcription" $
-          describe "toRNA" $ for_ cases test
+specs = describe "toRNA" $ for_ cases test
   where
     test Case{..} = it description $ toRNA dna `shouldBe` expected
 
