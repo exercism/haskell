c1d48b527b97771cb19cb03c09f4ba1b48edee10
rbasso <rbasso@sharpgeeks.net>
2017-05-10 14:49:09 +0900

    gigasecond: Remove test group with exercise's name

--- exercises/gigasecond/test/Tests.hs
+++ exercises/gigasecond/test/Tests.hs
@@ -19,20 +19,19 @@ main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
-specs = describe "gigasecond" $
-          describe "fromDay" $ do
+specs = describe "fromDay" $ do
 
-            let dt = readTime defaultTimeLocale
-                     (iso8601DateFormat (Just "%T%Z")) :: String -> UTCTime
+          let dt = readTime defaultTimeLocale
+                   (iso8601DateFormat (Just "%T%Z")) :: String -> UTCTime
 
-            it "from apr 25 2011" $
-              fromDay (dt "2011-04-25T00:00:00Z")
-              `shouldBe` dt "2043-01-01T01:46:40Z"
+          it "from apr 25 2011" $
+            fromDay (dt "2011-04-25T00:00:00Z")
+            `shouldBe` dt "2043-01-01T01:46:40Z"
 
-            it "from jun 13 1977" $
-              fromDay (dt "1977-06-13T00:00:00Z")
-              `shouldBe` dt "2009-02-19T01:46:40Z"
+          it "from jun 13 1977" $
+            fromDay (dt "1977-06-13T00:00:00Z")
+            `shouldBe` dt "2009-02-19T01:46:40Z"
 
-            it "from jul 19 1959" $
-              fromDay (dt "1959-07-19T00:00:00Z")
-              `shouldBe` dt "1991-03-27T01:46:40Z"
+          it "from jul 19 1959" $
+            fromDay (dt "1959-07-19T00:00:00Z")
+            `shouldBe` dt "1991-03-27T01:46:40Z"
