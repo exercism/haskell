dcef07f99c7cbe14f7cbe835271ce3e06ab22864
rbasso <rbasso@sharpgeeks.net>
2017-05-10 14:49:09 +0900

    all-your-base: Remove test group with exercise's name

--- exercises/all-your-base/test/Tests.hs
+++ exercises/all-your-base/test/Tests.hs
@@ -10,8 +10,7 @@ main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
-specs = describe "all-your-base" $
-          describe "rebase" $ for_ cases test
+specs = describe "rebase" $ for_ cases test
   where
 
     test Case{..} = it description assertion
