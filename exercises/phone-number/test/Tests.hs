0e9ab8bb065bc45fa5a421cf541818837394a55d
rbasso <rbasso@sharpgeeks.net>
2017-05-10 14:49:09 +0900

    phone-number: Remove test group with exercise's name

--- exercises/phone-number/test/Tests.hs
+++ exercises/phone-number/test/Tests.hs
@@ -10,8 +10,7 @@ main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
-specs = describe "phone-number" $
-          describe "number" $ for_ cases test
+specs = describe "number" $ for_ cases test
   where
     test Case{..} = it description $ number input `shouldBe` expected
 
