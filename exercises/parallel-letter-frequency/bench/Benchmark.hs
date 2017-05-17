f640f89a6df6fddb3cd571b224ca18438eca58ad
unmanbearpig <unmanbearpig@gmail.com>
2017-05-17 16:14:14 +0300

    parallel-letter-frequency: Add a benchmark
    I've added cases for running it on 4 and 500 anthems. Each number of
    anthems would run with a single worker and physical-number-of-cores
    number of workers. All cases run first on a single core and then on
    all available cores, as I found that results are often different.

---
+++ exercises/parallel-letter-frequency/bench/Benchmark.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Frequency (frequency)
+
+import Prelude hiding (unlines)
+import Data.Text (Text, unlines)
+
+import Criterion.Main (bench, bgroup, defaultMain, nf)
+import Criterion.Types (Benchmark)
+import Control.Concurrent (setNumCapabilities)
+import GHC.Conc (getNumProcessors)
+import Data.List (nub, sort, replicate)
+
+odeAnDieFreude :: Text
+odeAnDieFreude = unlines
+                 [ "Freude schöner Götterfunken"
+                 , "Tochter aus Elysium,"
+                 , "Wir betreten feuertrunken,"
+                 , "Himmlische, dein Heiligtum!"
+                 , "Deine Zauber binden wieder"
+                 , "Was die Mode streng geteilt;"
+                 , "Alle Menschen werden Brüder,"
+                 , "Wo dein sanfter Flügel weilt."
+                 ]
+
+
+makeBench :: [Text] -> Int -> Benchmark
+makeBench anthems workers = bench name $ nf (`frequency` anthems) workers
+  where name = show workers ++ " workers"
+
+benchGroup :: Int -> [Int] -> Int -> Benchmark
+benchGroup processors numWorkers numAnthems =
+  bgroup (show numAnthems ++ " anthems on " ++ show processors ++ " cores") (makeBench anthems <$> numWorkers)
+  where anthems = replicate numAnthems odeAnDieFreude
+
+main = do processors <- getNumProcessors
+          let numsOfWorkers = nub $ sort [1, processors]
+              numsOfAnthems = [4, 500]
+
+          -- run on 1 core
+          setNumCapabilities 1
+          defaultMain $ benchGroup 1 numsOfWorkers <$> numsOfAnthems
+
+          -- run on all cores
+          setNumCapabilities processors
+          defaultMain $ benchGroup processors numsOfWorkers <$> numsOfAnthems
