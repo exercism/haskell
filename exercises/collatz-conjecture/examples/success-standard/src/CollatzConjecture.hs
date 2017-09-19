2ed3db67b14a1bf243f580e23ad1c36792d62bd1
Average-user <lucaspolymeris@gmail.com>
2017-09-19 15:22:06 -0300

    handling errors with Maybe

--- exercises/collatz-conjecture/examples/success-standard/src/CollatzConjecture.hs
+++ exercises/collatz-conjecture/examples/success-standard/src/CollatzConjecture.hs
@@ -1,10 +1,9 @@
 module CollatzConjecture (collatz) where
 
-collatzHelper :: Integer -> Integer -> Integer
-collatzHelper t x | x == 1    = t
+collatzHelper :: Integer -> Integer -> Maybe Integer
+collatzHelper t x | x == 1    = Just t
                   | even x    = collatzHelper (t+1) (x `div` 2)
                   | otherwise = collatzHelper (t+1) (x*3 + 1)
 
-collatz :: Integer -> Integer
-collatz x = if x <= 0 then error "Only positive numbers are allowed"
-                      else collatzHelper 0 x
+collatz :: Integer -> Maybe Integer
+collatz x = if  x <= 0 then Nothing else collatzHelper 0 x
--- exercises/collatz-conjecture/test/Tests.hs
+++ exercises/collatz-conjecture/test/Tests.hs
@@ -16,44 +16,44 @@ specs = describe "collatz" $ for_ cases test
 
     test Case{..} = it description assertion
       where
-        assertion = collatz number `shouldBe` fromIntegral expected
+        assertion = collatz number `shouldBe` expected
 
 
 data Case = Case { description :: String
                  , property    :: String
                  , number      :: Integer
-                 , expected    :: Integer
+                 , expected    :: Maybe Integer
                  }
 
 cases :: [Case]
 cases = [ Case { description = "zero steps for one"
                , property    = "steps"
                , number      = 1
-               , expected    = 0
+               , expected    = Just 0
                }
         , Case { description = "divide if even"
                , property    = "steps"
                , number      = 16
-               , expected    = 4
+               , expected    = Just 4
                }
         , Case { description = "even and odd steps"
                , property    = "steps"
                , number      = 12
-               , expected    = 9
+               , expected    =Just 9
                }
         , Case { description = "Large number of even and odd steps"
                , property    = "steps"
                , number      = 1000000
-               , expected    = 152
+               , expected    = Just 152
                }
         , Case { description = "zero is an error"
                , property    = "steps"
                , number      = 0
-               , expected    = error "Only positive numbers are allowed"
+               , expected    = Nothing
                }
         , Case { description = "negative value is an error"
                , property    = "steps"
                , number      = -15
-               , expected    = error "Only positive numbers are allowed"
+               , expected    = Nothing
                }
         ]
