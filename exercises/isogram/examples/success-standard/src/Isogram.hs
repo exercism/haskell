ac9dd461bbdd0b99c4d0cea142a600bfa9d22230
Average-user <lucaspolymeris@gmail.com>
2017-09-20 02:26:53 -0300

    fix to isogram example

--- exercises/isogram/examples/success-standard/src/Isogram.hs
+++ exercises/isogram/examples/success-standard/src/Isogram.hs
@@ -5,4 +5,4 @@ import Data.Char (isLetter, toLower)
 isIsogram :: String -> Bool
 isIsogram = fn . map toLower
   where fn []     = True
-        fn (x:xs) = if isLetter x && x `elem` xs then False else fn xs
+        fn (x:xs) = not (isLetter x && x `elem` xs) && fn xs
