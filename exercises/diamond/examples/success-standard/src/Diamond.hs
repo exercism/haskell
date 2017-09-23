9ffa95fac4e9b230a85042a3adcfc8990c4e4164
Average-user <lucaspolymeris@gmail.com>
2017-09-22 21:28:13 -0300

    some corrections to Diamond.hs

--- exercises/diamond/examples/success-standard/src/Diamond.hs
+++ exercises/diamond/examples/success-standard/src/Diamond.hs
@@ -7,7 +7,7 @@ diamondH rb (c, (0, y)) = rb y ++ [c] ++ rb y
 diamondH rb (c, (x, y)) = rb y ++ [c] ++ rb x ++ [c] ++ rb y
 
 diamond :: Char -> [String]
-diamond = (\x -> x ++ (tail $ reverse $ x)) . f . abs . (64-) . ord
-  where rows x = zip (0: (take (x-1) [1,3..])) [x-1,x-2..0]
+diamond = (\x -> x ++ tail (reverse x)) . f . abs . (64-) . ord
+  where rows x = zip (0 : take (x-1) [1,3..]) [x-1,x-2..0]
         f      = map (diamondH rb) . zip (map chr [65..90]) . rows
         rb x   = replicate x ' '
