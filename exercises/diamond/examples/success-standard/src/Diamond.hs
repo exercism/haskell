2872bc0a81a009da139407583fb0f21460870c02
Average-user <lucaspolymeris@gmail.com>
2017-10-03 09:19:31 -0300

    some changes to make the example more clear

--- exercises/diamond/examples/success-standard/src/Diamond.hs
+++ exercises/diamond/examples/success-standard/src/Diamond.hs
@@ -2,12 +2,15 @@ module Diamond (diamond) where
 
 import Data.Char (ord, chr)
 
-diamondH :: (Int -> String) -> (Char, (Int, Int)) -> String
-diamondH rb (c, (0, y)) = rb y ++ [c] ++ rb y
-diamondH rb (c, (x, y)) = rb y ++ [c] ++ rb x ++ [c] ++ rb y
+rb :: Int -> String
+rb x = replicate x ' '
+
+helper :: (Char, (Int, Int)) -> String
+helper (c, (0, y)) = rb y ++ [c] ++ rb y
+helper (c, (x, y)) = rb y ++ [c] ++ rb x ++ [c] ++ rb y
 
 diamond :: Char -> [String]
-diamond = (\x -> x ++ tail (reverse x)) . f . abs . (64-) . ord
-  where rows x = zip (0 : take (x-1) [1,3..]) [x-1,x-2..0]
-        f      = map (diamondH rb) . zip (map chr [65..90]) . rows
-        rb x   = replicate x ' '
+diamond = (\x -> x ++ tail (reverse x)) . f . (subtract 64) . ord
+  where rows x = zip (0 : take (x-1) [1, 3..]) [x-1, x-2..0]
+        f      = map helper . zip abc . rows
+        abc    = (map chr [65..90])
