7179db0de3bff87abb0a32cf1bf27521e937a691
Average-user <lucaspolymeris@gmail.com>
2017-10-03 09:24:26 -0300

    suggestions by travis

--- exercises/diamond/examples/success-standard/src/Diamond.hs
+++ exercises/diamond/examples/success-standard/src/Diamond.hs
@@ -10,7 +10,7 @@ helper (c, (0, y)) = rb y ++ [c] ++ rb y
 helper (c, (x, y)) = rb y ++ [c] ++ rb x ++ [c] ++ rb y
 
 diamond :: Char -> [String]
-diamond = (\x -> x ++ tail (reverse x)) . f . (subtract 64) . ord
+diamond = (\x -> x ++ tail (reverse x)) . f . subtract 64 . ord
   where rows x = zip (0 : take (x-1) [1, 3..]) [x-1, x-2..0]
-        f      = map helper . zip abc . rows
-        abc    = (map chr [65..90])
+        f      = zipWith (curry helper) abc . rows
+        abc    = map chr [65..90]
