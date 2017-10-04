641cae6b2862d69163d532c9daa7198d3a681d07
Peter Tseng <pht24@cornell.edu>
2017-10-03 21:14:55 -0700

    diamond: f -> mkTop

--- exercises/diamond/examples/success-standard/src/Diamond.hs
+++ exercises/diamond/examples/success-standard/src/Diamond.hs
@@ -10,7 +10,7 @@ oneRow c (0, y) = pad y ++ [c] ++ pad y
 oneRow c (x, y) = pad y ++ [c] ++ pad x ++ [c] ++ pad y
 
 diamond :: Char -> [String]
-diamond = (\x -> x ++ tail (reverse x)) . f . subtract 64 . ord
+diamond = (\x -> x ++ tail (reverse x)) . mkTop . subtract 64 . ord
   where rows x = zip (0 : take (x-1) [1, 3..]) [x-1, x-2..0]
-        f      = zipWith oneRow abc . rows
+        mkTop  = zipWith oneRow abc . rows
         abc    = map chr [65..90]
