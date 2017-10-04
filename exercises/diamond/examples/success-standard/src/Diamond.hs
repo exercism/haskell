11735099cb4e14719cb0ea1c7913974d6a5357b5
Peter Tseng <pht24@cornell.edu>
2017-10-03 21:12:39 -0700

    diamond: helper -> oneRow

--- exercises/diamond/examples/success-standard/src/Diamond.hs
+++ exercises/diamond/examples/success-standard/src/Diamond.hs
@@ -5,12 +5,12 @@ import Data.Char (ord, chr)
 pad :: Int -> String
 pad x = replicate x ' '
 
-helper :: Char -> (Int, Int) -> String
-helper c (0, y) = pad y ++ [c] ++ pad y
-helper c (x, y) = pad y ++ [c] ++ pad x ++ [c] ++ pad y
+oneRow :: Char -> (Int, Int) -> String
+oneRow c (0, y) = pad y ++ [c] ++ pad y
+oneRow c (x, y) = pad y ++ [c] ++ pad x ++ [c] ++ pad y
 
 diamond :: Char -> [String]
 diamond = (\x -> x ++ tail (reverse x)) . f . subtract 64 . ord
   where rows x = zip (0 : take (x-1) [1, 3..]) [x-1, x-2..0]
-        f      = zipWith helper abc . rows
+        f      = zipWith oneRow abc . rows
         abc    = map chr [65..90]
