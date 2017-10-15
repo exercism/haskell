80e21960165e66581394ea01ddcf2582c0648ee8
Average-user <lucaspolymeris@gmail.com>
2017-10-15 20:13:39 -0300

    useless parenthesis

--- exercises/rotational-cipher/examples/success-standard/src/RotationalCipher.hs
+++ exercises/rotational-cipher/examples/success-standard/src/RotationalCipher.hs
@@ -5,5 +5,5 @@ import Data.List (elemIndex)
 rotate :: Int -> String -> String
 rotate r = map (f r ['a'..'z'] . f r ['A'..'Z'])
   where f x abc e = case elemIndex e abc of
-                      Nothing  -> e
-                      (Just a) -> abc!!mod (a+x) 26
+                      Nothing -> e
+                      Just a  -> abc!!mod (a+x) 26
