6356637bc24a1b00f3346733973789b7f4cc58f7
Samuel Henrique Oliveira da Silva <samosaara@gmail.com>
2017-11-23 19:14:49 -0200

    New way without copying

--- exercises/binary-search/examples/success-standard/src/BinarySearch.hs
+++ exercises/binary-search/examples/success-standard/src/BinarySearch.hs
@@ -1,18 +1,17 @@
 module BinarySearch (binarySearch) where
-import Data.Array (Array, (!), elems, listArray)
-import Data.Maybe (isJust, fromJust)
+import Data.Array (Array, (!))
   
-binarySearch :: (Eq a, Ord a) => Array Int a -> a -> Maybe Int
+binFind ::  Array Int Int -> Int -> Int -> Int -> Int
+binFind arr x lwr upr
+  | middleVal == x = midInd
+  | x > middleVal = binFind arr x (midInd + 1) upr
+  | otherwise = binFind arr x lwr (midInd - 1)
+  where midInd = abs (upr + lwr) `div` 2 
+        middleVal = arr ! midInd
+
+binarySearch :: Array Int Int -> Int -> Maybe Int
 binarySearch arr x
   | null arr = Nothing
   | length arr == 1 = if arr ! 0 == x then Just 0 else Nothing
   | x < (arr ! 0) || x > (arr ! (length arr - 1)) = Nothing
-  | otherwise = let contents = elems arr
-                    middle = length contents `div` 2
-                    (farr, sarr) = splitAt middle contents
-                    fhalf = listArray (0, length farr - 1) farr
-                    shalf = listArray (0, length sarr - 1) sarr
-                    target = if x >= head sarr then shalf else fhalf
-                    res = binarySearch target x in 
-                if isJust res && target == shalf
-                then Just (fromJust res + middle) else res
+  | otherwise = Just $ binFind arr x 0 $ length arr
