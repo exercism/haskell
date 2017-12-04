50b23a0438dfa8efd9cfe36a0e9db92820f6783d
Ryan Potts <rpottsoh@users.noreply.github.com>
2017-12-03 20:21:14 -0500

    bob 1.1.0.4: Add yelling question rule
    Closes https://github.com/exercism/problem-specifications/issues/943.
    
    It was possible for a message directed to Bob to be both a question and
    yelling, which meant it was unclear which rule should take effect.
    Resolve ambiguity by simply adding a rule for that case.
    
    https://github.com/exercism/problem-specifications/pull/1025

--- exercises/bob/examples/success-standard/src/Bob.hs
+++ exercises/bob/examples/success-standard/src/Bob.hs
@@ -1,16 +1,20 @@
 module Bob (responseFor) where
 import Data.Char (isSpace, isUpper, isAlpha)
 
-data Prompt = Silence | Yell | Question | Other
+data Prompt = Silence | YellQuestion | Yell | Question | Other
 
 classify :: String -> Prompt
 classify s | all isSpace s = Silence
-           | any isAlpha s && all isUpper (filter isAlpha s) = Yell
-           | '?' == last (filter (not . isSpace) s) = Question
+           | yell && question = YellQuestion
+           | yell = Yell
+           | question = Question
            | otherwise = Other
+           where yell = any isAlpha s && all isUpper (filter isAlpha s)
+                 question = '?' == last (filter (not . isSpace) s)
 
 response :: Prompt -> String
 response Silence = "Fine. Be that way!"
+response YellQuestion = "Calm down, I know what I'm doing!"
 response Yell = "Whoa, chill out!"
 response Question = "Sure."
 response Other = "Whatever."
