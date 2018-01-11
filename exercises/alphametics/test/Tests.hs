1dbff1494c4be8d3790d72782b722cec618797f9
Peter Tseng <pht24@cornell.edu>
2018-01-11 03:03:10 -0800

    alphametics 1.2.0.4: Add test with 199 addends
    1.1.0: new test
    https://github.com/exercism/problem-specifications/pull/1024
    1.1.1: description
    https://github.com/exercism/problem-specifications/pull/1045
    1.2.0: input object
    https://github.com/exercism/problem-specifications/pull/1035

--- exercises/alphametics/test/Tests.hs
+++ exercises/alphametics/test/Tests.hs
@@ -63,6 +63,11 @@ cases = [ Case { description = "puzzle with three letters"
 --               , expected     = Just [('A', 5), ('D', 3), ('E', 4), ('F', 7), ('G', 8),
 --                                      ('N', 0), ('O', 2), ('R', 1), ('S', 6), ('T', 9)]
 --               }
+--        , Case { description = "puzzle with ten letters and 199 addends"
+--               , puzzle      = "THIS + A + FIRE + THEREFORE + FOR + ALL + HISTORIES + I + TELL + A + TALE + THAT + FALSIFIES + ITS + TITLE + TIS + A + LIE + THE + TALE + OF + THE + LAST + FIRE + HORSES + LATE + AFTER + THE + FIRST + FATHERS + FORESEE + THE + HORRORS + THE + LAST + FREE + TROLL + TERRIFIES + THE + HORSES + OF + FIRE + THE + TROLL + RESTS + AT + THE + HOLE + OF + LOSSES + IT + IS + THERE + THAT + SHE + STORES + ROLES + OF + LEATHERS + AFTER + SHE + SATISFIES + HER + HATE + OFF + THOSE + FEARS + A + TASTE + RISES + AS + SHE + HEARS + THE + LEAST + FAR + HORSE + THOSE + FAST + HORSES + THAT + FIRST + HEAR + THE + TROLL + FLEE + OFF + TO + THE + FOREST + THE + HORSES + THAT + ALERTS + RAISE + THE + STARES + OF + THE + OTHERS + AS + THE + TROLL + ASSAILS + AT + THE + TOTAL + SHIFT + HER + TEETH + TEAR + HOOF + OFF + TORSO + AS + THE + LAST + HORSE + FORFEITS + ITS + LIFE + THE + FIRST + FATHERS + HEAR + OF + THE + HORRORS + THEIR + FEARS + THAT + THE + FIRES + FOR + THEIR + FEASTS + ARREST + AS + THE + FIRST + FATHERS + RESETTLE + THE + LAST + OF + THE + FIRE + HORSES + THE + LAST + TROLL + HARASSES + THE + FOREST + HEART + FREE + AT + LAST + OF + THE + LAST + TROLL + ALL + OFFER + THEIR + FIRE + HEAT + TO + THE + ASSISTERS + FAR + OFF + THE + TROLL + FASTS + ITS + LIFE + SHORTER + AS + STARS + RISE + THE + HORSES + REST + SAFE + AFTER + ALL + SHARE + HOT + FISH + AS + THEIR + AFFILIATES + TAILOR + A + ROOFS + FOR + THEIR + SAFE == FORTRESSES"
+--               , expected    = Just [('A', 1), ('E', 0), ('F', 5), ('H', 8), ('I', 7),
+--                                     ('L', 2), ('O', 6), ('R', 3), ('S', 4), ('T', 9)]
+--               }
 --        , Case { description  = "puzzle with multiplication"
 --               , puzzle       = "IF * DR == DORI"
 --               , expected     = Just [('I', 8), ('F', 2), ('D', 3), ('R', 9), ('O', 1)]
