e37d5c13076fcb1474b83542cd63b44a4c92ac45
Peter Tseng <pht24@cornell.edu>
2018-02-14 12:19:57 +0000

    list-ops 2.2.0.4: Use problem-specifications tests
    This involves:
    
    * Adding foldr tests (empty, non-empty, non-commutative)
      * We had some foldr tests but it is not immediately clear which of
        these traits they test.
    * Marking all other tests as track-specific
    
    Note that there was no specific effort made to use the exact same values
    as problem-specifications, only to ensure that the same tests (in
    intent) are tested.
    
    Version 2.2.0 (input object, no-op):
    https://github.com/exercism/problem-specifications/pull/1187
    Version 2.1.0 (use consistent syntax for function):
    https://github.com/exercism/problem-specifications/pull/1097
    Version 2.0.0 (commutative vs non-commutative):
    https://github.com/exercism/problem-specifications/pull/863
    Version 1.0.0 (no change, versioning added):
    https://github.com/exercism/problem-specifications/pull/690
    Initial data:
    https://github.com/exercism/problem-specifications/pull/521

--- exercises/list-ops/test/Tests.hs
+++ exercises/list-ops/test/Tests.hs
@@ -40,6 +40,7 @@ specs = do
         length ([] :: [Int]) `shouldBe` 0
       it "of non-empty list" $
         length [1 .. 4 :: Int] `shouldBe` 4
+      -- Track-specific test
       it "of large list" $
         length [1 .. big :: Int] `shouldBe` big
 
@@ -66,19 +67,30 @@ specs = do
         foldl' (+) (0 :: Int) [] `shouldBe` 0
       it "of non-empty list" $
         foldl' (+) (-3) [1 .. 4 :: Int] `shouldBe` 7
+      -- Track-specific test
       it "of huge list" $
         foldl' (+) 0 [1 .. big] `shouldBe` big * (big + 1) `div` 2
       it "with non-commutative function" $
         foldl' (-) 10 [1 .. 4 :: Int] `shouldBe` 0
+      -- Track-specific test
       it "is not just foldr . flip" $
         foldl' (flip (:)) [] "asdf" `shouldBe` "fdsa"
+      -- Track-specific test
       it "is accumulator-strict (use seq or BangPatterns)" $
         evaluate (foldl' (flip const) () [throw StrictException, ()])
         `shouldThrow` (== StrictException)
 
     describe "foldr" $ do
+      it "of empty list" $
+        foldr (*) (2 :: Int) [] `shouldBe` 2
+      it "of non-empty list" $
+        foldr (+) 5 [1 .. 4 :: Int] `shouldBe` 15
+      it "with non-commutative function" $
+        foldr div 5 [2, 5 :: Int] `shouldBe` 2
+      -- Track-specific test
       it "as id" $
         foldr (:) [] [1 .. big] `shouldBe` [1 .. big]
+      -- Track-specific test
       it "as append" $
         foldr (:) [100 .. big] [1 .. 99] `shouldBe` [1 .. big]
 
@@ -91,6 +103,7 @@ specs = do
         [1 .. 4 :: Int] ++ [] `shouldBe` [1 .. 4]
       it "of non-empty lists" $
         [1 .. 3] ++ [4, 5 :: Int] `shouldBe` [1 .. 5]
+      -- Track-specific test
       it "of large lists" $
         [1 .. big `div` 2] ++ [1 + big `div` 2 .. big] `shouldBe` [1 .. big]
 
@@ -99,5 +112,6 @@ specs = do
         concat ([] :: [[Int]]) `shouldBe` []
       it "of list of lists" $
         concat [[1, 2], [3], [], [4, 5, 6 :: Int]] `shouldBe` [1 .. 6]
+      -- Track-specific test
       it "of large list of small lists" $
         concat (map (:[]) [1 .. big]) `shouldBe` [1 .. big]
