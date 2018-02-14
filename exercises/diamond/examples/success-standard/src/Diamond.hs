e102159bdce08ae41b917d47c1509a4716f1b163
rbasso <rbasso@sharpgeeks.net>
2018-02-14 08:02:21 -0300

    diamond: Change return type to Maybe (#653)
    Previously, the function had undefined behaviour on non-letter inputs.
    Now we can specify that it should return Nothing.
    For now, intentionally no tests are added for Nothing.
    
    Closes #626.

--- exercises/diamond/examples/success-standard/src/Diamond.hs
+++ exercises/diamond/examples/success-standard/src/Diamond.hs
@@ -9,8 +9,8 @@ oneRow :: Char -> (Int, Int) -> String
 oneRow c (0, y) = pad y ++ [c] ++ pad y
 oneRow c (x, y) = pad y ++ [c] ++ pad x ++ [c] ++ pad y
 
-diamond :: Char -> [String]
-diamond = (\x -> x ++ tail (reverse x)) . mkTop . subtract 64 . ord
+diamond :: Char -> Maybe [String]
+diamond = Just . (\x -> x ++ tail (reverse x)) . mkTop . subtract 64 . ord
   where rows x = zip (0 : take (x-1) [1, 3..]) [x-1, x-2..0]
         mkTop  = zipWith oneRow abc . rows
         abc    = map chr [65..90]
