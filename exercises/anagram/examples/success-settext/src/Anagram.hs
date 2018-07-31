4c48af8ddc4e0e733e8c5ac22f0b5e4349cc3d30
Peter Tseng <pht24@cornell.edu>
2018-07-31 23:55:58 +0000

    anagram: Use sort instead of multiset
    No more multiset in 12.4:
    https://www.stackage.org/diff/lts-11.19/lts-12.4

--- exercises/anagram/examples/success-settext/src/Anagram.hs
+++ exercises/anagram/examples/success-settext/src/Anagram.hs
@@ -1,7 +1,7 @@
 module Anagram (anagramsFor) where
 
 import Data.Function  (on)
-import Data.MultiSet  (fromList)
+import Data.List      (sort)
 import Data.Set       (Set, filter)
 import Data.Text      (Text, toLower, unpack)
 import Prelude hiding (filter)
@@ -10,4 +10,4 @@ anagramsFor :: Text -> Set Text -> Set Text
 anagramsFor x = filter (\w -> x `isDistinctOf` w && x `isAnagramOf` w)
   where
     isDistinctOf = (/=) `on` toLower
-    isAnagramOf  = (==) `on` fromList . unpack . toLower
+    isAnagramOf  = (==) `on` sort . unpack . toLower
