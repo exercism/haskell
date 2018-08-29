d51c53cbeb7ce24fe6a70bc9e03f91c962d65949
Bentbib Rachad <bentbibrachad@gmail.com>
2018-08-29 01:34:06 +0100

    difference-of-squares: Rename squareOfSums to squareOfSum (#711)
    difference-of-squares 1.2.0.6
    
    The name `squareOfSums` is a misnomer, as it returns the square of a
    single sum.
    
    The name `squareOfSum` is more accurate.

--- exercises/difference-of-squares/src/Squares.hs
+++ exercises/difference-of-squares/src/Squares.hs
@@ -1,10 +1,10 @@
-module Squares (difference, squareOfSums, sumOfSquares) where
+module Squares (difference, squareOfSum, sumOfSquares) where
 
 difference :: Integral a => a -> a
 difference n = error "You need to implement this function."
 
-squareOfSums :: Integral a => a -> a
-squareOfSums n = error "You need to implement this function."
+squareOfSum :: Integral a => a -> a
+squareOfSum n = error "You need to implement this function."
 
 sumOfSquares :: Integral a => a -> a
 sumOfSquares n = error "You need to implement this function."
