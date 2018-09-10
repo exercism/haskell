a5d65fe85649476859485a9c686f59c11de59bc0
Simon Shine <shreddedglory@gmail.com>
2018-09-10 15:03:31 +0200

    nucleotide-count: Use data type to represent nucleotides (#714)
    nucleotide-count 1.3.0.6
    
    Nucleotides are better represented as a disjoint union not a char.
    Char may bias toward using strings more often than advisable.
    
    Fixes https://github.com/exercism/haskell/issues/694

--- exercises/nucleotide-count/test/Tests.hs
+++ exercises/nucleotide-count/test/Tests.hs
@@ -5,7 +5,7 @@ import Data.Map          (fromList)
 import Test.Hspec        (Spec, describe, it, shouldBe, shouldSatisfy)
 import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
 
-import DNA (nucleotideCounts)
+import DNA (nucleotideCounts, Nucleotide(..))
 
 main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
@@ -18,29 +18,29 @@ specs = do
           describe "nucleotideCounts" $ do
 
             it "empty dna strand has no nucleotides" $
-              nucleotideCounts "" `matchesMap` [ ('A', 0)
-                                               , ('C', 0)
-                                               , ('G', 0)
-                                               , ('T', 0) ]
+              nucleotideCounts "" `matchesMap` [ (A, 0)
+                                               , (C, 0)
+                                               , (G, 0)
+                                               , (T, 0) ]
 
             it "can count one nucleotide in single-character input" $
-              nucleotideCounts "G" `matchesMap` [ ('A', 0)
-                                                , ('C', 0)
-                                                , ('G', 1)
-                                                , ('T', 0) ]
+              nucleotideCounts "G" `matchesMap` [ (A, 0)
+                                                , (C, 0)
+                                                , (G, 1)
+                                                , (T, 0) ]
 
             it "repetitive-sequence-has-only-guanosine" $
-              nucleotideCounts "GGGGGGGG" `matchesMap` [ ('A', 0)
-                                                       , ('C', 0)
-                                                       , ('G', 8)
-                                                       , ('T', 0) ]
+              nucleotideCounts "GGGGGGGG" `matchesMap` [ (A, 0)
+                                                       , (C, 0)
+                                                       , (G, 8)
+                                                       , (T, 0) ]
 
             it "counts all nucleotides" $
               nucleotideCounts "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
-              `matchesMap` [ ('A', 20)
-                           , ('C', 12)
-                           , ('G', 17)
-                           , ('T', 21) ]
+              `matchesMap` [ (A, 20)
+                           , (C, 12)
+                           , (G, 17)
+                           , (T, 21) ]
 
             it "validates strand" $
               nucleotideCounts "AGXXACT" `shouldSatisfy` isLeft
