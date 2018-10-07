37e30a49e1430e1f81e05a10562d7067e81d7d8b
Average-user <lucaspolymeris@pm.me>
2018-10-07 12:28:14 -0300

    mod -> rem, div -> quot

--- exercises/armstrong-numbers/examples/success-standard/src/ArmstrongNumbers.hs
+++ exercises/armstrong-numbers/examples/success-standard/src/ArmstrongNumbers.hs
@@ -2,7 +2,7 @@ module ArmstrongNumbers (armstrong) where
 
 digits :: Integral x => x -> [x]
 digits 0 = []
-digits x = x `mod` 10 : digits (x `div` 10)
+digits x = x `rem` 10 : digits (x `quot` 10)
 
 armstrong :: Integral a => a -> Bool
 armstrong n = n == sum (map (^ length ds) ds)
