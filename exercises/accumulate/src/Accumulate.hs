4572e08e146c5eb462de01c8225fffb33365e4de
Jan Seeger <jan.seeger@siemens.com>
2018-11-27 14:38:51 +0100

    Changed exercise for test.

--- exercises/accumulate/src/Accumulate.hs
+++ exercises/accumulate/src/Accumulate.hs
@@ -1,4 +1,4 @@
 module Accumulate (accumulate) where
 
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate f xs = error "You need to implement this function."
+accumulate f xs = error "You need to implement this thing."
