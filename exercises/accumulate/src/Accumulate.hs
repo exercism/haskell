7fc476c5dbd9caf02f445d3b64e356f662cf5da2
Jan Seeger <jan.seeger@siemens.com>
2018-11-28 13:36:25 +0100

    Changed exercise for test.

--- exercises/accumulate/src/Accumulate.hs
+++ exercises/accumulate/src/Accumulate.hs
@@ -1,4 +1,4 @@
 module Accumulate (accumulate) where
 
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate f xs = error "You needoeu aoerc  to implement this oreidaoeir ."
+accumulate f xs = error "You needa oeuo oeu aoerc  to implement this oreidaoeir ."
