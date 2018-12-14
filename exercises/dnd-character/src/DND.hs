be093ec0da3ca3bc91188616e03972c746f914cd
Simon Shine <shreddedglory@gmail.com>
2018-12-14 20:53:18 +0100

    dnd-character: add new exercise (#782)
    This exercise was created in exercism/problem-specifications#1397.
    
    It is a basic exercise in random generators, so the idea I had for the
    Haskell track was to practice making QuickCheck generators rather than
    simply use the standard random generator.
    
    For this reason, QuickCheck is added as a root dependency.
    
    The 1.1.0 test case added in exercism/problem-specifications#1416 does
    not apply to QuickCheck generators.

---
+++ exercises/dnd-character/src/DND.hs
@@ -0,0 +1,31 @@
+module DND ( Character(..)
+           , ability
+           , modifier
+           , character
+           ) where
+
+import Test.QuickCheck (Gen)
+
+data Character = Character
+  { name         :: String
+  , strength     :: Int
+  , dexterity    :: Int
+  , constitution :: Int
+  , intelligence :: Int
+  , wisdom       :: Int
+  , charisma     :: Int
+  , hitpoints    :: Int
+  }
+  deriving (Show, Eq)
+
+modifier :: Int -> Int
+modifier =
+  error "You need to implement this function."
+
+ability :: Gen Int
+ability =
+  error "You need to implement this generator."
+
+character :: Gen Character
+character =
+  error "You need to implement this generator."
