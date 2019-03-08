0b7f37ecfbc73575b969d5b971c4d4b20dba88ce
Simon Shine <shreddedglory@gmail.com>
2019-03-08 11:39:29 +0100

    binary-search: Make find_ accept tuple in example solution
    This avoids uncurrying.

--- exercises/binary-search/examples/success-standard/src/BinarySearch.hs
+++ exercises/binary-search/examples/success-standard/src/BinarySearch.hs
@@ -3,13 +3,13 @@ module BinarySearch (find) where
 import Data.Array (Array, (!), bounds)
 
 find :: Ord a => Array Int a -> a -> Maybe Int
-find array value = uncurry find_ $ bounds array
+find array value = find_ $ bounds array
   where
-    find_ :: Int -> Int -> Maybe Int
-    find_ i j
+    find_ :: (Int, Int) -> Maybe Int
+    find_ (i, j)
       | i > j                 = Nothing
-      | value < array ! pivot = find_ i (pivot-1)
-      | value > array ! pivot = find_ (pivot+1) j
+      | value < array ! pivot = find_ (i, pivot-1)
+      | value > array ! pivot = find_ (pivot+1, j)
       | otherwise             = Just pivot
       where
         pivot = (i + j) `div` 2
