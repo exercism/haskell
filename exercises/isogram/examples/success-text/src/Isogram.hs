6549a65598112cde0004522c7801f54348d10f0b
Simon Shine <shreddedglory@gmail.com>
2019-03-13 08:46:04 +0100

    Remove unused OverloadedStrings pragma, refactored slightly

--- exercises/isogram/examples/success-text/src/Isogram.hs
+++ exercises/isogram/examples/success-text/src/Isogram.hs
@@ -1,23 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Isogram (isIsogram) where
 
-import qualified Data.Char as C
-import qualified Data.Maybe as M
-import qualified Data.Set as S
-import           Data.Set (Set)
-import qualified Data.Text as T
-import           Data.Text (Text)
-
+import Data.Char (isLetter)
+import Data.Maybe (isJust)
 import Data.MonoTraversable (ofoldM)
+import Data.Set (Set, empty, member, insert)
+import Data.Text (Text)
+import qualified Data.Text as T
 
 isIsogram :: Text -> Bool
-isIsogram = M.isJust . isogram
-
-isogram :: Text -> Maybe (Set Char)
-isogram = ofoldM gather S.empty . T.toLower
+isIsogram = isJust . ofoldM gather empty . T.toLower . T.filter isLetter
   where
     gather :: Set Char -> Char -> Maybe (Set Char)
     gather seen c
-      | c `S.member` seen = Nothing
-      | not (C.isLetter c) = return seen
-      | otherwise = return (S.insert c seen)
+      | c `member` seen = Nothing
+      | otherwise = return (insert c seen)
