0567caea80ea6c8edfef5b66979ca34cace7ae88
Simon Shine <shreddedglory@gmail.com>
2019-03-26 09:29:41 +0100

    sublist: Replace 'Sublist' type with 'Maybe Ordering' (fixes #814) (#815)
    * sublist: Replace 'Sublist' type with 'Maybe Ordering' (fixes #814)
    
    Expand hint to describe return type `Maybe Ordering`.

--- exercises/sublist/examples/success-standard/src/Sublist.hs
+++ exercises/sublist/examples/success-standard/src/Sublist.hs
@@ -1,15 +1,9 @@
-module Sublist (Sublist(..), sublist) where
+module Sublist (sublist) where
 import Data.List (isInfixOf)
 
-data Sublist = Equal
-             | Sublist
-             | Superlist
-             | Unequal
-             deriving (Eq, Show)
-
-sublist :: Eq a => [a] -> [a] -> Sublist
+sublist :: Eq a => [a] -> [a] -> Maybe Ordering
 sublist xs ys
-  | xs == ys          = Equal
-  | xs `isInfixOf` ys = Sublist
-  | ys `isInfixOf` xs = Superlist
-  | otherwise         = Unequal
+  | xs == ys          = Just EQ
+  | xs `isInfixOf` ys = Just LT
+  | ys `isInfixOf` xs = Just GT
+  | otherwise         = Nothing
