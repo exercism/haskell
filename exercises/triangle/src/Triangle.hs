e3f0a0448c578fec4ba9ba11bbfee41fc5e9fdba
Simon Shine <shreddedglory@gmail.com>
2019-06-18 15:33:18 +0200

    triangle: Add `Ord` constraint to handout stub
    As @phlummox commented in 9f2d160753fc0cb0dbc2536cd937a4eb9978114f,
    
    > [...] adding a type signature to triangleType seems to suggest [...]
    > that the problem can be solved by implementing a function of that type.
    
    As we cannot think of a solution with only `Num` and not either `Ord` or
    `Eq`, we add `Ord` to the handout stub as this provides most flexibility.

--- exercises/triangle/src/Triangle.hs
+++ exercises/triangle/src/Triangle.hs
@@ -6,5 +6,5 @@ data TriangleType = Equilateral
                   | Illegal
                   deriving (Eq, Show)
 
-triangleType :: Num a => a -> a -> a -> TriangleType
+triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
 triangleType a b c = error "You need to implement this function."
