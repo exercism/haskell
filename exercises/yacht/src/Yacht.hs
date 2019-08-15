0a4ca6217662ea3f40fff996fefcfc95a6c90939
guy <strings.stringandstrings@gmail.com>
2019-08-15 10:32:24 -0400

    yacht: added parameter names to stub file

--- exercises/yacht/src/Yacht.hs
+++ exercises/yacht/src/Yacht.hs
@@ -14,4 +14,4 @@ data Category = Ones
               | Yacht
 
 yacht :: Category -> [Int] -> Int
-yacht = error "You need to implement this function."
+yacht category dice = error "You need to implement this function."
