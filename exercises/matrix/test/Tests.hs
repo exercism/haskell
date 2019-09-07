da85faa3fea30f25eb7c5a7038886bb20402bd00
Peter Tseng <petertseng@users.noreply.github.com>
2019-09-07 09:08:25 +0000

    matrix 1.1.0.6: Use 1-based indexing (#838)
    Matrix indices are usually defined so that (1, 1) is the top left entry,
    not (0, 0). See for example any linear algebra textbook or wikipedia:
    https://en.wikipedia.org/wiki/Matrix_(mathematics)#/media/File:Matrix.svg
    
    https://github.com/exercism/problem-specifications/pull/1387
    
    Matches Data.Matrix.getElem:
    https://hackage.haskell.org/package/matrix/docs/Data-Matrix.html#v:getElem

--- exercises/matrix/test/Tests.hs
+++ exercises/matrix/test/Tests.hs
@@ -28,28 +28,28 @@ specs = do
     let vector = Vector.fromList
 
     it "extract row from one number matrix" $
-      row 0 (intMatrix "1") `shouldBe` vector [1]
+      row 1 (intMatrix "1") `shouldBe` vector [1]
 
     it "can extract row" $
-      row 1 (intMatrix "1 2\n3 4") `shouldBe` vector [3, 4]
+      row 2 (intMatrix "1 2\n3 4") `shouldBe` vector [3, 4]
 
     it "extract row where numbers have different widths" $
-      row 1 (intMatrix "1 2\n10 20") `shouldBe` vector [10, 20]
+      row 2 (intMatrix "1 2\n10 20") `shouldBe` vector [10, 20]
 
     it "can extract row from non-square matrix" $
-      row 2 (intMatrix "1 2 3\n4 5 6\n7 8 9\n8 7 6") `shouldBe` vector [7, 8, 9]
+      row 3 (intMatrix "1 2 3\n4 5 6\n7 8 9\n8 7 6") `shouldBe` vector [7, 8, 9]
 
     it "extract column from one number matrix" $
-      column 0 (intMatrix "1") `shouldBe` vector [1]
+      column 1 (intMatrix "1") `shouldBe` vector [1]
 
     it "can extract column" $
-      column 2 (intMatrix "1 2 3\n4 5 6\n7 8 9") `shouldBe` vector [3, 6, 9]
+      column 3 (intMatrix "1 2 3\n4 5 6\n7 8 9") `shouldBe` vector [3, 6, 9]
 
     it "can extract column from non-square matrix" $
-      column 2 (intMatrix "1 2 3\n4 5 6\n7 8 9\n8 7 6") `shouldBe` vector [3, 6, 9, 6]
+      column 3 (intMatrix "1 2 3\n4 5 6\n7 8 9\n8 7 6") `shouldBe` vector [3, 6, 9, 6]
 
     it "extract column where numbers have different widths" $
-      column 1 (intMatrix "89 1903 3\n18 3 1\n9 4 800") `shouldBe` vector [1903, 3, 4]
+      column 2 (intMatrix "89 1903 3\n18 3 1\n9 4 800") `shouldBe` vector [1903, 3, 4]
 
     -- Track-specific tests:
 
