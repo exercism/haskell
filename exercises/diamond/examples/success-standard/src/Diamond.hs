d26899407341e9cd0457972574387c891b89b1fd
Simon Shine <shreddedglory@gmail.com>
2019-09-27 12:39:54 +0200

    Fix example solution after adding property-based tests
    Because @barrymoo's property-based tests uncovered a bug in the example
    solution, the example solution is replaced with one that passes current
    tests.

--- exercises/diamond/examples/success-standard/src/Diamond.hs
+++ exercises/diamond/examples/success-standard/src/Diamond.hs
@@ -2,15 +2,11 @@ module Diamond (diamond) where
 
 import Data.Char (ord, chr)
 
-pad :: Int -> String
-pad x = replicate x ' '
-
-oneRow :: Char -> (Int, Int) -> String
-oneRow c (0, y) = pad y ++ [c] ++ pad y
-oneRow c (x, y) = pad y ++ [c] ++ pad x ++ [c] ++ pad y
-
 diamond :: Char -> Maybe [String]
-diamond = Just . (\x -> x ++ tail (reverse x)) . mkTop . subtract 64 . ord
-  where rows x = zip (0 : take (x-1) [1, 3..]) [x-1, x-2..0]
-        mkTop  = zipWith oneRow abc . rows
-        abc    = map chr [65..90]
+diamond c | c `notElem` ['A'..'Z'] = Nothing
+diamond c = Just . mirror . map row $ [0..n] where
+  n = ord c - ord 'A'
+  mirror top = top ++ reverse (init top)
+  row i = mirror $ spaces (n - i) ++ [letter i] ++ spaces i
+  letter i = chr (ord 'A' + i)
+  spaces i = replicate i ' '
