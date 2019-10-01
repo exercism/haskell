7d720ca98d9b2cd508862af0a735f6b4e8157266
Barry Moore <moore0557@gmail.com>
2019-10-01 00:40:16 -0400

    Diamond: Implement property-based tests (#843)
    Diamond's test suite is extended with a set of property-based tests.
    
    These are in part inspired by the list of properties from README.md, and in
    part by Mark Seemann's blog post on property-based checks for exactly this
    exercise:
    
        https://blog.ploeh.dk/2015/01/10/diamond-kata-with-fscheck/
    
    Only a subset of the properties mentioned are implemented.  This leaves room
    for students to extend the test suite further, although exercism/exercism#5052
    and #847 indicate that we may not wish to encourage this in the near future.
    
    Because @barrymoo's property-based tests uncovered a bug in the example
    solution, the example solution is replaced with one that passes current tests.

--- exercises/diamond/test/Tests.hs
+++ exercises/diamond/test/Tests.hs
@@ -1,17 +1,53 @@
 {-# LANGUAGE RecordWildCards #-}
 
+import Data.Char         (isLetter, isPrint, isSpace)
 import Data.Foldable     (for_)
+import Data.List         (isSuffixOf)
+import Data.Maybe        (isJust, isNothing)
 import Test.Hspec        (Spec, describe, it, shouldBe)
 import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+import Test.QuickCheck   (arbitraryASCIIChar, conjoin, counterexample,
+                          discard, elements, forAll, forAllShrink, Gen,
+                          Property, suchThat, Testable, (===))
+
 import Diamond (diamond)
 
 main :: IO ()
 main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
-specs = describe "diamond" $ for_ cases test
-  where
+specs = describe "diamond" $ do
+  it "should not have a result for a non-alpha character" $
+    forAllShrink genNonAlphaChar shrinkNonAlphaChar $
+      isNothing . diamond
 
+  it "should produce a value for an alpha character" $
+    forAll genAlphaChar $ isJust . diamond
+
+  it "should have an odd number of rows" $
+    forAllDiamond $ odd . length
+
+  it "should have equal top and bottom" $
+    forAllDiamond $ \rows ->
+      let halfRoundDown = length rows `div` 2
+      in take halfRoundDown rows === take halfRoundDown (reverse rows)
+
+  it "should have the same width and height" $
+    forAllDiamond $ \rows ->
+      let sameHeightWidth idx row = counterexample
+            (concat [ "The length of row with index "
+                    , show idx
+                    , " is not equal to the height" ])
+            (length row === length rows)
+      in conjoin $ zipWith sameHeightWidth [0 :: Int ..] rows
+
+  it "rows should start and end with the same letter" $
+    forAllDiamond $
+      let headEqualsLast row = not (null row) && take 1 row `isSuffixOf` row
+      in (&&) <$> not . null <*> all (headEqualsLast . filter (not . isSpace))
+
+  for_ cases test
+  where
     test Case{..} = it description assertion
       where
         assertion = diamond input `shouldBe` Just expected
@@ -107,3 +143,23 @@ cases = [ Case { description = "Degenerate case with a single 'A' row"
                         "                         A                         "]
                }
         ]
+
+genNonAlphaChar :: Gen Char
+genNonAlphaChar = arbitraryASCIIChar `suchThat` (not . isLetter)
+
+genAlphaChar :: Gen Char
+genAlphaChar = elements ['A'..'Z']
+
+genDiamond :: Gen (Maybe [String])
+genDiamond = diamond <$> genAlphaChar
+
+forAllDiamond :: Testable prop => ([String] -> prop) -> Property
+forAllDiamond p = forAll genDiamond $ maybe discard p
+
+shrinkNonAlphaChar :: Char -> String
+shrinkNonAlphaChar c =
+  if isPrint c
+  then takeWhile (/= c) printableChars
+  else printableChars
+  where
+    printableChars = filter isPrint ['\0' .. '\127']
