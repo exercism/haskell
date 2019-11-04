826de2a1f93f75ed074208305c5588128b0128f3
Simon Shine <shreddedglory@gmail.com>
2019-11-04 14:17:05 +0100

    Rename 'value1' back to 'convert'
    This makes the PR less invasive.

--- exercises/resistor-color-duo/examples/success-standard/src/ResistorColors.hs
+++ exercises/resistor-color-duo/examples/success-standard/src/ResistorColors.hs
@@ -13,17 +13,17 @@ data Color =
   | White
   deriving (Eq, Show)
 
-value1 :: Color -> Int
-value1 Black = 0
-value1 Brown = 1
-value1 Red = 2
-value1 Orange = 3
-value1 Yellow = 4
-value1 Green = 5
-value1 Blue = 6
-value1 Violet = 7
-value1 Grey = 8
-value1 White = 9
+convert :: Color -> Int
+convert Black = 0
+convert Brown = 1
+convert Red = 2
+convert Orange = 3
+convert Yellow = 4
+convert Green = 5
+convert Blue = 6
+convert Violet = 7
+convert Grey = 8
+convert White = 9
 
 value :: (Color, Color) -> Int
-value (a, b) = 10 * value1 a + value1 b
+value (a, b) = 10 * convert a + convert b
