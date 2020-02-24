3c4b17ba012b31821c108dd0b57be9c2d220a2ce
Nicuvëo <crucuny@gmail.com>
2020-02-24 20:25:05 +0000

    bank-account: fixed example from Int to Integer

--- exercises/bank-account/examples/success-standard/src/BankAccount.hs
+++ exercises/bank-account/examples/success-standard/src/BankAccount.hs
@@ -3,7 +3,7 @@ module BankAccount ( BankAccount
                    , getBalance, incrementBalance) where
 import Control.Concurrent.STM (TVar, atomically, newTVar, readTVar, readTVarIO, writeTVar)
 
-newtype BankAccount = BankAccount { unBankAccount :: TVar (Maybe Int) }
+newtype BankAccount = BankAccount { unBankAccount :: TVar (Maybe Integer) }
 
 openAccount :: IO BankAccount
 openAccount = atomically $ BankAccount <$> newTVar (Just 0)
@@ -11,10 +11,10 @@ openAccount = atomically $ BankAccount <$> newTVar (Just 0)
 closeAccount :: BankAccount -> IO ()
 closeAccount = atomically . flip writeTVar Nothing . unBankAccount
 
-getBalance :: BankAccount -> IO (Maybe Int)
+getBalance :: BankAccount -> IO (Maybe Integer)
 getBalance = readTVarIO . unBankAccount
 
-incrementBalance :: BankAccount -> Int -> IO (Maybe Int)
+incrementBalance :: BankAccount -> Integer -> IO (Maybe Integer)
 incrementBalance acct delta = atomically $ do
   let b = unBankAccount acct
   bal <- fmap (delta +) <$> readTVar b
