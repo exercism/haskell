c9422d6024cc8963477345886ee18d05f24886d1
Peter Tseng <pht24@cornell.edu>
2020-11-17 03:59:45 +0000

    travis: move three shell scripts to GitHub Actions
    ensure readme updated
    check conflet fmt
    ensure stack resolvers synced
    (install yq)
    
    https://github.com/exercism/haskell/issues/921
    
    The Exercism organisation is moving to GitHub Actions.
    It runs faster.

--- bin/ensure-stack-resolvers-synced.sh
+++ bin/ensure-stack-resolvers-synced.sh
@@ -3,9 +3,10 @@
 # This ensures that each exercise's stack resolver has the same version.
 
 differing_stack=""
-expected_stack=$(grep 'RESOLVER.*CURRENT' .travis.yml | head -1 | cut -d'"' -f2)
+first_stack_yaml=$(ls -1 exercises/*/stack.yaml | head -1)
+expected_stack=$(yq read "$first_stack_yaml" resolver)
 echo "All exercises should have resolver $expected_stack"
-for exercise in ${TRAVIS_BUILD_DIR}/exercises/*/ ; do
+for exercise in $(git rev-parse --show-toplevel)/exercises/*/ ; do
     exercise_stack=$(yq read "$exercise/stack.yaml" resolver)
     if ! [ "$exercise_stack" = "$expected_stack" ]; then
       differing_stack="$differing_stack $(basename "$exercise")"
