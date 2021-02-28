2e3002b82f3c1c7de2021b52b5fdcc9d35c0c584
Simon Shine <ssh@penneo.com>
2021-02-28 22:55:38 +0100

    Unbreak CI: There is no such thing as README.md
    Rename the script into
    
    ```
    bin/ensure-practice-exercise-descriptions-are-synced.sh
    ```
    
    since what was once exercises are now called practice exercises. That
    which is called concept exercises is not shared between tracks, and so
    syncing their descriptions is not well-defined.
    
    This commit preserves the `override_message`:
    
    ```
    override_message="I have confirmed ... no README check ..."
    ```
    
    (censored to avoid triggering CI) in order to not pick a new slogan.
    
    While exercism/configlet#179 is being resolved, the only thing we can
    test is that each practice exercise has an instructions.md, which is the
    only description file that is always present after #950.

---
+++ bin/ensure-practice-exercise-descriptions-are-synced.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# Check that practice exercise descriptions are in sync with problem-specifications.
+
+override_message="I have confirmed that no README check is needed"
+
+# If *any* commit message contains the override message:
+if git log origin/main..HEAD | grep -q "$override_message"; then
+  echo "WARNING: You've overridden the README check, which applies to ALL commits in this PR."
+  echo "No practice exercise description in this PR will be checked for changes."
+  exit 0
+fi
+
+newline=$'\n  '
+
+changed_practice_exercises() {
+  branch_name="$(git rev-parse --abbrev-ref HEAD)"
+  git diff --name-only "origin/main..$branch_name" | \
+    grep -Po '(?<=^exercises/practice/)[-a-z]+' | sort -fu
+}
+
+missing_readmes=""
+for exercise in $(changed_practice_exercises); do
+  echo "Checking readme for $exercise"
+
+  instructions_path="exercises/practice/$exercise/.docs/instructions.md"
+  if [ ! -f "$instructions_path" ]; then
+    missing_readmes="$missing_readmes$newline$exercise"
+  fi
+done
+
+if [ -n "$missing_readmes" ]; then
+  echo "Exercises missing instructions.md:$missing_readmes"
+fi
+if [ -n "$missing_readmes" ]; then
+   exit 1
+fi
