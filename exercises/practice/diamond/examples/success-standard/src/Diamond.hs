89ad3cd7683bea3510ac7779722c5f970756ade4
Peter Tseng <pht24@cornell.edu>
2021-09-16 20:03:35 +0000

    diamond: hlint: use tail . reverse instead of reverse . init

--- exercises/practice/diamond/examples/success-standard/src/Diamond.hs
+++ exercises/practice/diamond/examples/success-standard/src/Diamond.hs
@@ -6,7 +6,7 @@ diamond :: Char -> Maybe [String]
 diamond c | c `notElem` ['A'..'Z'] = Nothing
 diamond c = Just . mirror . map row $ [0..n] where
   n = ord c - ord 'A'
-  mirror top = top ++ reverse (init top)
+  mirror top = top ++ tail (reverse top)
   row i = mirror $ spaces (n - i) ++ [letter i] ++ spaces i
   letter i = chr (ord 'A' + i)
   spaces i = replicate i ' '
