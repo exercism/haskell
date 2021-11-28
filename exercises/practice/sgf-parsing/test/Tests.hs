28bd50abacfa37fd4a0900004d4d11d41062335c
Peter Tseng <pht24@cornell.edu>
2021-11-28 15:57:45 +0000

    sgf-parsing: Explain all current test cases
    It may otherwise be too cryptic why some of these things are errors
    
    https://github.com/exercism/haskell/pull/1025

--- exercises/practice/sgf-parsing/test/Tests.hs
+++ exercises/practice/sgf-parsing/test/Tests.hs
@@ -21,18 +21,30 @@ specs = describe "parseSgf" $ for_ cases test
         assertion   = parseSgf input `shouldBe` fmap fromList <$> expected
 
     cases = [ (""                                   , Nothing                                                )
+            -- error: tree with no nodes
             , ("()"                                 , Nothing                                                )
+            -- error: node without tree
             , (";"                                  , Nothing                                                )
+            -- error: node without properties
             , ("(;)"                                , Just  $ Node [] []                                     )
+            -- single-node tree
             , ("(;A[B])"                            , Just  $ Node [("A", ["B"])] []                         )
+            -- multiple properties
             , ("(;A[b]C[d])"                        , Just  $ Node [("A", ["b"]), ("C", ["d"])] []           )
+            -- error: properties without value
             , ("(;A)"                               , Nothing                                                )
+            -- error: lowercase property identifier
             , ("(;a[b])"                            , Nothing                                                )
+            -- error: mixed-case property identifier
             , ("(;Aa[b])"                           , Nothing                                                )
+            -- two nodes
             , ("(;A[B];B[C])"                       , Just  $ Node [("A", ["B"])] [ Node [("B", ["C"])] [] ] )
+            -- two child trees
             , ("(;A[B](;B[C])(;C[D]))"              , Just  $ Node [("A", ["B"])] [ Node [("B", ["C"])] []
                                                                                   , Node [("C", ["D"])] [] ] )
+            -- multiple property values
             , ("(;A[b][c][d])"                      , Just  $ Node [("A", ["b", "c", "d" ])] []              )
+            -- escaped property value
             , ("(;A[\\]b\nc\\\nd\t\te\\\\ \\\n\\]])", Just  $ Node [("A", ["]b cd  e\\ ]"])] []              ) ]
 
 -- b74debc3be24b5c81650189935c9bbfa019b367e
