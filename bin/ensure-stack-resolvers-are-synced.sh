57d8fd4d7090d5dcb921cc5a489ba9cde46ed144
Peter Tseng <pht24@cornell.edu>
2021-12-08 03:05:06 +0000

    ensure-stack-resolvers-are-synced: check concept too

--- bin/ensure-stack-resolvers-are-synced.sh
+++ bin/ensure-stack-resolvers-are-synced.sh
@@ -6,11 +6,13 @@ differing_stack=""
 first_stack_yaml=$(ls -1 exercises/practice/*/stack.yaml | head -1)
 expected_stack=$(yq eval .resolver "$first_stack_yaml")
 echo "All exercises should have resolver $expected_stack"
-for exercise in $(git rev-parse --show-toplevel)/exercises/practice/*/ ; do
+for etype in concept practice; do
+  for exercise in $(git rev-parse --show-toplevel)/exercises/${etype}/*/ ; do
     exercise_stack=$(yq eval .resolver "$exercise/stack.yaml")
     if ! [ "$exercise_stack" = "$expected_stack" ]; then
-      differing_stack="$differing_stack $(basename "$exercise")"
+      differing_stack="$differing_stack ${etype}/$(basename "$exercise")"
     fi
+  done
 done
 if [ -n "$differing_stack" ]; then
     echo "The following exercises have a different stack.yaml resolver:$differing_stack"
