5525cd2a91e8f1b6373e251ec48467e5e4d449a6
PWadsworth <wadsworthpt@gmail.com>
2022-08-25 18:07:31 -0400

    Correct hlint warnings

--- exercises/concept/pacman-rules/test/Tests.hs
+++ exercises/concept/pacman-rules/test/Tests.hs
@@ -5,64 +5,64 @@ main :: IO ()
 main = hspec $ do
   describe "eatsGhost" $ do
     it "ghost eaten when touched while powered" $ do
-      (eatsGhost True True) `shouldBe` True
+      eatsGhost True True `shouldBe` True
 
     it "ghost not eaten when not touched" $ do
-      (eatsGhost True False) `shouldBe` False
+      eatsGhost True False `shouldBe` False
 
     it "ghost not eaten when touched un-powered" $ do
-      (eatsGhost False True) `shouldBe` False
+      eatsGhost False True `shouldBe` False
 
     it "ghost not eaten when not touched while un-powered" $ do
-      (eatsGhost False False) `shouldBe` False
+      eatsGhost False False `shouldBe` False
 
   describe "scores" $ do
     it "when eating dot" $ do
-      (scores False True) `shouldBe` True
+      scores False True `shouldBe` True
 
     it "when eating power pellet" $ do
-      (scores True False) `shouldBe` True
+      scores True False `shouldBe` True
 
     it "nothing when nothing eaten" $ do
-      (scores False False) `shouldBe` False
+      scores False False `shouldBe` False
 
     it "when eating power pellet and dot" $ do
-      (scores True True) `shouldBe` True
+      scores True True `shouldBe` True
 
   describe "loses" $ do
     it "if touching ghost un-powered " $ do
-      (loses False True) `shouldBe` True
+      loses False True `shouldBe` True
 
     it "is False if touching ghost powered" $ do
-      (loses True True) `shouldBe` False
+      loses True True `shouldBe` False
 
     it "is False if not touching a ghost powered" $ do
-      (loses True False) `shouldBe` False
+      loses True False `shouldBe` False
 
     it "is False if not touching a ghost un-powered" $ do
-      (loses False False) `shouldBe` False
+      loses False False `shouldBe` False
 
   describe "wins" $ do
     it "if has eaten all dots and not touching a ghost un-powered" $ do
-      (wins True False False) `shouldBe` True
+      wins True False False `shouldBe` True
 
     it "if has eaten all dots and touching a ghost powered" $ do
-      (wins True True True) `shouldBe` True
+      wins True True True `shouldBe` True
 
     it "if has eaten all dots and not touching ghost powered" $ do
-      (wins True True False) `shouldBe` True
+      wins True True False `shouldBe` True
 
     it "is False if has eaten all dots and touching ghost un-powered" $ do
-      (wins True False True) `shouldBe` False
+      wins True False True `shouldBe` False
 
     it "is False if has not eaten all dots and not touching a ghost un-powered" $ do
-      (wins False False False) `shouldBe` False
+      wins False False False `shouldBe` False
 
     it "is False if has not eaten all dots and touching a ghost powered" $ do
-      (wins False True True) `shouldBe` False
+      wins False True True `shouldBe` False
 
     it "is False if has not eaten all dots and not touching ghost powered" $ do
-      (wins False True False) `shouldBe` False
+      wins False True False `shouldBe` False
 
     it "is False if has not eaten all dots and touching ghost un-powered" $ do
-      (wins False False True) `shouldBe` False
+      wins False False True `shouldBe` False
