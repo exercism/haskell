e4c8ad614e2cf0b5c77aba4d9ec5f1c958a203eb
Katrina Owen <katrina.owen@gmail.com>
2022-08-27 22:19:21 +0200

    Add concept exercise Pac-Man Rules for booleans
    Co-authored-by: PWadsworth <wadsworthpt@gmail.com>
    Co-authored-by:  Matthijs <19817960+MatthijsBlom@users.noreply.github.com>

---
+++ exercises/concept/pacman-rules/src/PacmanRules.hs
@@ -0,0 +1,17 @@
+module PacmanRules (eatsGhost, scores, loses, wins) where
+
+eatsGhost :: Bool -> Bool -> Bool
+eatsGhost =
+  error "Implement this function."
+
+scores :: Bool -> Bool -> Bool
+scores =
+  error "Implement this function."
+
+loses :: Bool -> Bool -> Bool
+loses =
+  error "Implement this function."
+
+wins :: Bool -> Bool -> Bool -> Bool
+wins =
+  error "Implement this function."
