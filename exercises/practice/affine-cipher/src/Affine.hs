8ef5f5b1319a7ebae3a2196ecccf660730053dd8
tofische <tofische@users.noreply.github.com>
2024-02-01 20:55:09 +0100

    Add Affine Cipher exercise

---
+++ exercises/practice/affine-cipher/src/Affine.hs
@@ -0,0 +1,7 @@
+module Affine (decode, encode) where
+
+decode :: (Int, Int) -> String -> Maybe String
+decode key cipherText = error "You need to implement this function."
+
+encode :: (Int, Int) -> String -> Maybe String
+encode key plainText = error "You need to implement this function."
