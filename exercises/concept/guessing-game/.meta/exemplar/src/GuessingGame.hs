833981ce1af752138a4516ae53d95db6c5eb71d9
tofische <tofische@users.noreply.github.com>
2026-08-09 16:37:51 +0200

    Correct test cases.

--- exercises/concept/guessing-game/.meta/exemplar/src/GuessingGame.hs
+++ exercises/concept/guessing-game/.meta/exemplar/src/GuessingGame.hs
@@ -3,7 +3,7 @@ module GuessingGame (reply) where
 reply :: Int -> Int -> String
 reply n guess
   | guess == n = "Correct"
-  | guess + 1 == n || guess - 1 == n = "So close!"
+  | guess + 1 == n || guess - 1 == n = "So close"
   | guess < n = "Too low"
   | guess > n = "Too high"
   | otherwise = "Incorrect" -- catch-all clause to make pattern matching exhaustive
--- exercises/concept/guessing-game/test/Tests.hs
+++ exercises/concept/guessing-game/test/Tests.hs
@@ -9,12 +9,12 @@ specs :: Spec
 specs = do
   describe "reply" $ do
     it "1 should be 'Too low'" $
-      reply 1 `shouldBe` "Too low"
+      reply 42 1 `shouldBe` "Too low"
     it "100 should be 'Too high'" $
-      reply 100 `shouldBe` "Too high"
+      reply 42 100 `shouldBe` "Too high"
     it "41 should be 'So close'" $
-      reply 41 `shouldBe` "So close"
+      reply 42 41 `shouldBe` "So close"
     it "43 should be 'So close'" $
-      reply 43 `shouldBe` "So close"
+      reply 42 43 `shouldBe` "So close"
     it "42 should be 'Correct'" $
-      reply 42 `shouldBe` "Correct"
+      reply 42 42 `shouldBe` "Correct"
